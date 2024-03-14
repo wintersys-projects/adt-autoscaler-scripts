d61ac0c104c2ece0704c8de4c85a931f2e98ace2
wintersys-projects <160187405+wintersys-projects@users.noreply.github.com>
2024-03-14 17:26:38 +0000

    Update InstallSysStat.sh

--- installscripts/InstallSysStat.sh
+++ installscripts/InstallSysStat.sh
@@ -24,13 +24,16 @@ then
     buildos="${1}"
 fi
 
-if ( [ "${buildos}" = "ubuntu" ] )
+if ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
-    DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sysstat
-fi
+    if ( [ "${buildos}" = "ubuntu" ] )
+    then
+        DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sysstat
+    fi
 
-if ( [ "${buildos}" = "debian" ] )
-then
-    DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sysstat
+    if ( [ "${buildos}" = "debian" ] )
+    then
+        DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sysstat
+    fi
 fi
 
