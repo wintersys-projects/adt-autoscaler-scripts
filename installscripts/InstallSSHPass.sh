eaa85beac6614d2aede5000179b59316cdb8c271
wintersys-projects <160187405+wintersys-projects@users.noreply.github.com>
2024-03-14 17:25:43 +0000

    Update InstallSSHPass.sh

--- installscripts/InstallSSHPass.sh
+++ installscripts/InstallSSHPass.sh
@@ -24,13 +24,16 @@ then
     buildos="${1}"
 fi
 
-if ( [ "${buildos}" = "ubuntu" ] )
+if ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
-    DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sshpass
-fi
+    if ( [ "${buildos}" = "ubuntu" ] )
+    then
+        DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sshpass
+    fi
 
-if ( [ "${buildos}" = "debian" ] )
-then
-    DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sshpass
+    if ( [ "${buildos}" = "debian" ] )
+    then
+        DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sshpass
+    fi
 fi
 
