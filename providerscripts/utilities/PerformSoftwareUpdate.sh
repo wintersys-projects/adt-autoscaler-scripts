4c33498663419742796210e3d0c6f21b553d2d68
wintersys-projects <160187405+wintersys-projects@users.noreply.github.com>
2024-03-14 11:33:35 +0000

    Update PerformSoftwareUpdate.sh

--- providerscripts/utilities/PerformSoftwareUpdate.sh
+++ providerscripts/utilities/PerformSoftwareUpdate.sh
@@ -22,7 +22,7 @@ BUILDOS="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDOS'`"
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDOS:ubuntu`" = "1" ] || [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDOS:debian`" = "1" ] )
 then
-     ${HOME}/installscripts/Update.sh ${BUILDOS}
+     ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}   
      
      if ( [ -f ${HOME}/EC2 ] )
      then
