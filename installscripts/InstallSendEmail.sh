2035970a3a4d3875b4efe70af26134c1d6c5e7b1
wintersys-projects <160187405+wintersys-projects@users.noreply.github.com>
2024-03-14 17:26:10 +0000

    Update InstallSendEmail.sh

--- installscripts/InstallSendEmail.sh
+++ installscripts/InstallSendEmail.sh
@@ -24,13 +24,16 @@ then
     buildos="${1}"
 fi
 
-if ( [ "${buildos}" = "ubuntu" ] )
+if ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
-    DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sendemail
-fi
+    if ( [ "${buildos}" = "ubuntu" ] )
+    then
+        DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sendemail
+    fi
 
-if ( [ "${buildos}" = "debian" ] )
-then
-    DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sendemail
+    if ( [ "${buildos}" = "debian" ] )
+    then
+        DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq install sendemail
+    fi
 fi
 
