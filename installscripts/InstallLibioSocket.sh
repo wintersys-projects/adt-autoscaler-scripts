ab61a722bb3b6d58ffb4de67365205a08465b6cc
wintersys-projects <160187405+wintersys-projects@users.noreply.github.com>
2024-03-15 14:17:16 +0000

    Update InstallLibioSocket.sh

--- installscripts/InstallLibioSocket.sh
+++ installscripts/InstallLibioSocket.sh
@@ -37,12 +37,12 @@ if ( [ "${apt}" != "" ] )
 then
     if ( [ "${buildos}" = "ubuntu" ] )
     then
-        DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq install libio-socket-ssl-perl
+        DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install libio-socket-ssl-perl
     fi
 
     if ( [ "${buildos}" = "debian" ] )
     then
-        DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq install libio-socket-ssl-perl
+        DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install libio-socket-ssl-perl
     fi
 fi
 
