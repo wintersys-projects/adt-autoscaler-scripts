e86a42e9d6a1bd1c6d1f1f2c783f9f2e89e664b0
wintersys-projects <160187405+wintersys-projects@users.noreply.github.com>
2024-03-16 19:01:51 +0000

    Update GetProxyDNSIPs.sh

--- providerscripts/security/firewall/GetProxyDNSIPs.sh
+++ providerscripts/security/firewall/GetProxyDNSIPs.sh
@@ -104,22 +104,6 @@ then
         alldnsproxyips="${autoscalerips} ${alldnsproxyips}"
         alldnsproxyips="`/bin/echo ${alldnsproxyips} | /bin/sed 's/"//g'`"
     fi
-
-    if ( [ "${CLOUDHOST}" = "aws" ] )
-    then
-        #dynamic
-        alldnsproxyips="\"`/usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/ips" | /usr/bin/jq '.result.ipv4_cidrs' | /bin/grep '"' | /bin/sed 's/.* "//g' | /bin/sed 's/".*//g' | /usr/bin/tr '\n' ' '| /bin/sed 's/ $//g'`\""
-        
-        if ( [ "${alldnsproxyips}" = "" ] || [ "${alldnsproxyips}" = "null" ] )
-        then
-            #hardcoded
-            alldnsproxyips="103.21.244.0/22 103.22.200.0/22 103.31.4.0/22 104.16.0.0/13 104.24.0.0/14 108.162.192.0/18 141.101.64.0/18 162.158.0.0/15 172.64.0.0/13 173.245.48.0/20 188.114.96.0/20 190.93.240.0/20 197.234.240.0/22 198.41.128.0/17 131.0.72.0/22 199.27.128.0/21"
-        fi
-        
-        autoscalerips="`/bin/echo ${autoscalerips} | /bin/sed 's/,/ /g'`"
-        alldnsproxyips="${autoscalerips} ${alldnsproxyips}"
-        alldnsproxyips="`/bin/echo ${alldnsproxyips} | /bin/sed 's/"//g'`"
-    fi
 fi
 
 alldnsproxyips="`/bin/echo ${alldnsproxyips} | /bin/sed 's/,$//g'`"
