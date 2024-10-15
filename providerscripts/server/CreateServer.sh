b3918243ba377bb248fa959a7f7a38b212602289
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-15 11:52:11 +0100

    Update CreateServer.sh

--- providerscripts/server/CreateServer.sh
+++ providerscripts/server/CreateServer.sh
@@ -150,7 +150,7 @@ then
 	fi
 
   	vpc_id="`/usr/local/bin/linode-cli --text vpcs list | /bin/grep adt-vpc | /usr/bin/awk '{print $1}'`"
-	subnet_id="`/usr/local/bin/linode-cli --text vpcs subnets-list ${vpc_id} | /bin/grep adt-subnet | /usr/bin/awk '{print $1}'`"
+	subnet_id="`/usr/local/bin/linode-cli --text vpcs subnets-list ${vpc_id} | /bin/grep 10.0.1 | /bin/grep adt-subnet | /usr/bin/awk '{print $1}'`"
 	
 	#Linode supports snapshots, so decide if we are building from a snapshot
 	if ( [ "${snapshot_id}" != "" ] && [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh SNAPAUTOSCALE:1`" = "1" ] )
