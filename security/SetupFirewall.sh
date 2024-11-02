41fcb7cb4bff3f43ae496ed0a87cb42054ab8d39
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-02 20:30:41 +0000

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -39,12 +39,23 @@ then
 	exit
 fi
 
-if ( [ -f ${HOME}/runtime/FIREWALL-ACTIVE ] && [ "`/usr/bin/ufw status | /bin/grep 'inactive'`" = "" ] )
+firewall=""
+if ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "FIREWALL" | /usr/bin/awk -F':' '{print $NF}'`" = "ufw" ] )
 then
-	exit
+	firewall="ufw"
+elif ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "FIREWALL" | /usr/bin/awk -F':' '{print $NF}'`" = "iptables" ] )
+then
+	firewall="iptables"
 fi
 
-/usr/sbin/ufw logging off
+if ( [ "${firewall}" = "ufw" ] )
+then
+	if ( [ -f ${HOME}/runtime/FIREWALL-ACTIVE ] && [ "`/usr/bin/ufw status | /bin/grep 'inactive'`" = "" ] )
+	then
+		exit
+	fi
+	/usr/sbin/ufw logging off
+fi
 
 SSH_PORT="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'SSHPORT'`"
 CLOUDHOST="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'CLOUDHOST'`"
@@ -59,62 +70,83 @@ updated="0"
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDMACHINEVPC:0`" = "1" ] )
 then
-	if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep ${BUILD_CLIENT_IP} | /bin/grep ALLOW`" = "" ] )
+	if ( [ "${firewall}" = "ufw" ] )
 	then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from ${BUILD_CLIENT_IP} to any port ${SSH_PORT}
-		/bin/sleep 5
-		updated="1"
+		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep ${BUILD_CLIENT_IP} | /bin/grep ALLOW`" = "" ] )
+		then
+			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from ${BUILD_CLIENT_IP} to any port ${SSH_PORT}
+			/bin/sleep 5
+			updated="1"
+		fi
 	fi
 fi
 
 if ( [ "${CLOUDHOST}" = "digitalocean" ] )
 then
-   if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.116.0.0/24" | /bin/grep ALLOW`" = "" ] )
-   then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.116.0.0/24 to any port ${SSH_PORT}
-		/bin/sleep 5
-		updated="1"
-	fi
+	if ( [ "${firewall}" = "ufw" ] )
+	then
+   		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.116.0.0/24" | /bin/grep ALLOW`" = "" ] )
+   		then
+			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.116.0.0/24 to any port ${SSH_PORT}
+			/bin/sleep 5
+			updated="1"
+		fi
+  	fi
 fi
 
 if ( [ "${CLOUDHOST}" = "exoscale" ] )
 then
-   if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.0.0.0/24" | /bin/grep ALLOW`" = "" ] )
-   then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.0.0.0/24 to any port ${SSH_PORT}
-		/bin/sleep 5
-		updated="1"
+	if ( [ "${firewall}" = "ufw" ] )
+	then
+   		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.0.0.0/24" | /bin/grep ALLOW`" = "" ] )
+   		then
+			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.0.0.0/24 to any port ${SSH_PORT}
+			/bin/sleep 5
+			updated="1"
+		fi
 	fi
 fi
 
 if ( [ "${CLOUDHOST}" = "linode" ] )
 then
-   if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.0.1.0/24" | /bin/grep ALLOW`" = "" ] )
-   then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.0.1.0/24 to any port ${SSH_PORT}
-		/bin/sleep 5
-		updated="1"
+	if ( [ "${firewall}" = "ufw" ] )
+	then
+   		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.0.1.0/24" | /bin/grep ALLOW`" = "" ] )
+   		then
+			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.0.1.0/24 to any port ${SSH_PORT}
+			/bin/sleep 5
+			updated="1"
+		fi
 	fi
 fi
 
 if ( [ "${CLOUDHOST}" = "vultr" ] )
 then
-	if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "192.168.0.0/16" | /bin/grep ALLOW`" = "" ] )
+	if ( [ "${firewall}" = "ufw" ] )
 	then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 192.168.0.0/16 to any port ${SSH_PORT}
-		/bin/sleep 5
-		updated="1"
+		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "192.168.0.0/16" | /bin/grep ALLOW`" = "" ] )
+		then
+			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 192.168.0.0/16 to any port ${SSH_PORT}
+			/bin/sleep 5
+			updated="1"
+		fi
 	fi
 fi
 
 if ( [ "${updated}" = "1" ] )
 then
-	/usr/sbin/ufw -f enable
-	/bin/sleep 5
-	/usr/sbin/service networking restart
+	if ( [ "${firewall}" = "ufw" ] )
+	then
+		/usr/sbin/ufw -f enable
+		/bin/sleep 5
+		/usr/sbin/service networking restart
+	fi
 fi
 
-if ( [ "`/usr/bin/ufw status | /bin/grep 'inactive'`" = "" ] )
+if ( [ "${firewall}" = "ufw" ] )
 then
-	/bin/touch ${HOME}/runtime/FIREWALL-ACTIVE
+	if ( [ "`/usr/bin/ufw status | /bin/grep 'inactive'`" = "" ] )
+	then
+		/bin/touch ${HOME}/runtime/FIREWALL-ACTIVE
+	fi
 fi
