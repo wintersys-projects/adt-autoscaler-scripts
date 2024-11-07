6fd29ac40e028c2f15bbc56272ab21f0cbb89b7a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-07 13:13:34 +0000

    Update CheckConfigDatastore.sh

--- providerscripts/datastore/configwrapper/CheckConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/CheckConfigDatastore.sh
@@ -33,10 +33,12 @@ fi
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-   if ( [ "`/usr/bin/s3cmd ls s3://${configbucket}/$1`" = "" ] )
-   then
-	   /bin/echo "0"
-   else
-	   /bin/echo "1"
-   fi
+        datastore_tool="/usr/bin/s3cmd"
+fi
+
+if ( [ "`${datastore_tool} ls s3://${configbucket}/$1`" = "" ] )
+then
+	/bin/echo "0"
+else
+	/bin/echo "1"
 fi
