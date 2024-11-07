ad558d8bc309d19c57d09451b619f7f36f29b367
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-07 13:21:39 +0000

    Update ObtainBuildClientIP.sh

--- providerscripts/datastore/ObtainBuildClientIP.sh
+++ providerscripts/datastore/ObtainBuildClientIP.sh
@@ -43,8 +43,11 @@ BUILD_IDENTIFIER="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUIL
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-	/usr/bin/s3cmd get s3://adt-${BUILD_IDENTIFIER}/* ${HOME}/runtime/BUILDCLIENTIP
+        datastore_tool="/usr/bin/s3cmd"
 fi
+
+${datastore_tool} get s3://adt-${BUILD_IDENTIFIER}/* ${HOME}/runtime/BUILDCLIENTIP
+
 
 BUILD_CLIENT_IP="`/bin/ls ${HOME}/runtime/BUILDCLIENTIP/* | /usr/bin/awk -F'/' '{print $NF}' | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`"
 OLD_BUILD_CLIENT_IP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDCLIENTIP'`"
