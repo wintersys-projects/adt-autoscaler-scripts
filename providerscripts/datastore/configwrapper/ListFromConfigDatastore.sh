ccc0aef2c5b16fe289b9c56a2a4f4ba0913d2613
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-08 00:55:04 +0000

    Update ListFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh
@@ -28,19 +28,19 @@ configbucket="${configbucket}-config"
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-        datastore_tool="/usr/bin/s3cmd --recursive ls "
+        datastore_tool="/usr/bin/s3cmd "
 	datastore_tool_1="/usr/bin/s3cmd ls "
 elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
         host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} ls "
-	datastore_tool_1="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} ls "
+        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} "
 fi
 
-if ( [ "${2}" = "recursive" ] )
+value="`${datastore_tool} ls s3://${configbucket}/$1 | /usr/bin/awk -F'/' '{print $NF}' | /bin/sed '/^$/d'`"
+if ( [ "`/bin/echo ${value} | /usr/bin/wc -w`" != "1" ] )
 then
-	${datastore_tool_1} s3://${configbucket}/$1 | /usr/bin/awk -F'/' '{print $NF}' | /bin/sed '/^$/d'
+        ${datastore_tool} ls s3://${configbucket}/$1 | /usr/bin/awk -F' ' '{print $NF}' | /bin/sed '/^$/d'
 else
-	${datastore_tool} s3://${configbucket}/$1 | /usr/bin/awk -F'/' '{print $NF}' | /bin/sed '/^$/d'
+        /bin/echo ${value}
 fi
 
