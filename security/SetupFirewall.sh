82e33ab96e45f48d22cf462cd8c50b61229edd83
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-13 15:19:00 +0000

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -102,89 +102,119 @@ then
         fi
 fi
 
-if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+
+if ( [ "${firewall}" = "ufw" ] )
 then
-	if ( [ "${firewall}" = "ufw" ] )
+	if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "${VPC_IP_RANGE}" | /bin/grep ALLOW`" = "" ] )
+   	then
+		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from ${VPC_IP_RANGE} to any port ${SSH_PORT}
+		/bin/sleep 5
+		updated="1"
+	fi
+elif ( [ "${firewall}" = "iptables" ] )
+then
+	if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${SSH_PORT} | /bin/grep ${VPC_IP_RANGE}`" = "" ] )
 	then
-   		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.116.0.0/24" | /bin/grep ALLOW`" = "" ] )
-   		then
-			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.116.0.0/24 to any port ${SSH_PORT}
-			/bin/sleep 5
-			updated="1"
-		fi
-        elif ( [ "${firewall}" = "iptables" ] )
-        then
-                if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${SSH_PORT} | /bin/grep 10.116.0.0`" = "" ] )
-                then
-                        /usr/sbin/iptables -I INPUT -s 10.116.0.0/24 -p tcp --dport ${SSH_PORT} -j ACCEPT
-                        /usr/sbin/iptables -I INPUT -s 10.116.0.0/24 -p ICMP --icmp-type 8 -j ACCEPT
-                        updated="1"
-                fi
-        fi
+		/usr/sbin/iptables -I INPUT -s ${VPC_IP_RANGE} -p tcp --dport ${SSH_PORT} -j ACCEPT
+		/usr/sbin/iptables -I INPUT -s ${VPC_IP_RANGE} -p ICMP --icmp-type 8 -j ACCEPT
+		updated="1"
+	fi
 fi
 
-if ( [ "${CLOUDHOST}" = "exoscale" ] )
-then
-	if ( [ "${firewall}" = "ufw" ] )
-	then
-   		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.0.0.0/24" | /bin/grep ALLOW`" = "" ] )
-   		then
-			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.0.0.0/24 to any port ${SSH_PORT}
-			/bin/sleep 5
-			updated="1"
-		fi
-        elif ( [ "${firewall}" = "iptables" ] )
-        then
-                if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${SSH_PORT} | /bin/grep 10.0.0.0`" = "" ] )
-                then
-                        /usr/sbin/iptables -I INPUT -s 10.0.0.0/24 -p tcp --dport ${SSH_PORT} -j ACCEPT
-                        /usr/sbin/iptables -I INPUT -s 10.0.0.0/24 -p ICMP --icmp-type 8 -j ACCEPT
-                        updated="1"
-                fi
-        fi
-fi
 
-if ( [ "${CLOUDHOST}" = "linode" ] )
-then
-	if ( [ "${firewall}" = "ufw" ] )
-	then
-   		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.0.1.0/24" | /bin/grep ALLOW`" = "" ] )
-   		then
-			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.0.1.0/24 to any port ${SSH_PORT}
-			/bin/sleep 5
-			updated="1"
-		fi
-        elif ( [ "${firewall}" = "iptables" ] )
-        then
-                if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${SSH_PORT} | /bin/grep 10.0.1.0`" = "" ] )
-                then
-                        /usr/sbin/iptables -I INPUT -s 10.0.1.0/24 -p tcp --dport ${SSH_PORT} -j ACCEPT
-                        /usr/sbin/iptables -I INPUT -s 10.0.1.0/24 -p ICMP --icmp-type 8 -j ACCEPT
-                        updated="1"
-                fi
-        fi
-fi
 
-if ( [ "${CLOUDHOST}" = "vultr" ] )
-then
-	if ( [ "${firewall}" = "ufw" ] )
-	then
-		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "192.168.0.0/16" | /bin/grep ALLOW`" = "" ] )
-		then
-			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 192.168.0.0/16 to any port ${SSH_PORT}
-			/bin/sleep 5
-			updated="1"
-		fi
-        elif ( [ "${firewall}" = "iptables" ] )
-        then
-                if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${SSH_PORT} | /bin/grep 192.168.0.0`" = "" ] )
-                then
-                        /usr/sbin/iptables -I INPUT -s 192.168.0.0/16 -p tcp --dport ${SSH_PORT} -j ACCEPT
-                        /usr/sbin/iptables -I INPUT -s 192.168.0.0/16 -p ICMP --icmp-type 8 -j ACCEPT
-                        updated="1"
-                fi
-        fi
-fi
+
+
+
+
+
+
+
+
+
+#if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+#then
+#	if ( [ "${firewall}" = "ufw" ] )
+#	then
+#   		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.116.0.0/24" | /bin/grep ALLOW`" = "" ] )
+ #  		then
+#			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.116.0.0/24 to any port ${SSH_PORT}
+#			/bin/sleep 5
+#			updated="1"
+#		fi
+ #       elif ( [ "${firewall}" = "iptables" ] )
+  #      then
+  #              if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${SSH_PORT} | /bin/grep 10.116.0.0`" = "" ] )
+  #              then
+  #                      /usr/sbin/iptables -I INPUT -s 10.116.0.0/24 -p tcp --dport ${SSH_PORT} -j ACCEPT
+  #                      /usr/sbin/iptables -I INPUT -s 10.116.0.0/24 -p ICMP --icmp-type 8 -j ACCEPT
+  #                      updated="1"
+  #              fi
+  #      fi
+#fi
+
+#if ( [ "${CLOUDHOST}" = "exoscale" ] )
+#then
+#	if ( [ "${firewall}" = "ufw" ] )
+#	then
+ #  		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.0.0.0/24" | /bin/grep ALLOW`" = "" ] )
+  # 		then
+#			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.0.0.0/24 to any port ${SSH_PORT}
+#			/bin/sleep 5
+#			updated="1"
+#		fi
+ #       elif ( [ "${firewall}" = "iptables" ] )
+  #      then
+   #             if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${SSH_PORT} | /bin/grep 10.0.0.0`" = "" ] )
+    #            then
+     #                   /usr/sbin/iptables -I INPUT -s 10.0.0.0/24 -p tcp --dport ${SSH_PORT} -j ACCEPT
+      #                  /usr/sbin/iptables -I INPUT -s 10.0.0.0/24 -p ICMP --icmp-type 8 -j ACCEPT
+      #                  updated="1"
+      #          fi
+      #  fi
+#fi
+
+#if ( [ "${CLOUDHOST}" = "linode" ] )
+#then
+#	if ( [ "${firewall}" = "ufw" ] )
+#	then
+ #  		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "10.0.1.0/24" | /bin/grep ALLOW`" = "" ] )
+  # 		then
+#			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 10.0.1.0/24 to any port ${SSH_PORT}
+#			/bin/sleep 5
+#			updated="1"
+#		fi
+ #       elif ( [ "${firewall}" = "iptables" ] )
+  #      then
+   #             if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${SSH_PORT} | /bin/grep 10.0.1.0`" = "" ] )
+    #            then
+     #                   /usr/sbin/iptables -I INPUT -s 10.0.1.0/24 -p tcp --dport ${SSH_PORT} -j ACCEPT
+      #                  /usr/sbin/iptables -I INPUT -s 10.0.1.0/24 -p ICMP --icmp-type 8 -j ACCEPT
+       #                 updated="1"
+    #            fi
+  #      fi
+#fi
+
+#if ( [ "${CLOUDHOST}" = "vultr" ] )
+#then
+#	if ( [ "${firewall}" = "ufw" ] )
+#	then
+#		if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep "192.168.0.0/16" | /bin/grep ALLOW`" = "" ] )
+#		then
+#			/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from 192.168.0.0/16 to any port ${SSH_PORT}
+#			/bin/sleep 5
+#			updated="1"
+#		fi
+ #       elif ( [ "${firewall}" = "iptables" ] )
+  #      then
+   #             if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${SSH_PORT} | /bin/grep 192.168.0.0`" = "" ] )
+    #            then
+     #                   /usr/sbin/iptables -I INPUT -s 192.168.0.0/16 -p tcp --dport ${SSH_PORT} -j ACCEPT
+      #                  /usr/sbin/iptables -I INPUT -s 192.168.0.0/16 -p ICMP --icmp-type 8 -j ACCEPT
+      #                  updated="1"
+      #          fi
+      #  fi
+#fi
 
 if ( [ "${updated}" = "1" ] )
 then
