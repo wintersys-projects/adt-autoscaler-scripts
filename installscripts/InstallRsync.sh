d89176a523135799caa701f380e65ab146e66262
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-17 10:19:05 +0000

    Update InstallRsync.sh

--- installscripts/InstallRsync.sh
+++ installscripts/InstallRsync.sh
@@ -37,11 +37,11 @@ if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install rsync
+		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install rsync	#####UBUNTU-RSYNC-REPO#####
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install rsync   
+		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install rsync   #####DEBIAN-GLANCES-REPO#####
 	fi
 fi
