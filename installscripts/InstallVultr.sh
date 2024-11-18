c37ca295ca7fa5d14aaaa7a9b743be55de32296b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-18 10:07:02 +0000

    Update InstallVultr.sh

--- installscripts/InstallVultr.sh
+++ installscripts/InstallVultr.sh
@@ -1,25 +1,4 @@
 #!/bin/sh
-###############################################################################################
-# Description: This script will install vultr toolkit
-# Author: Peter Winter
-# Date: 12/01/2017
-###############################################################################################
-# License Agreement:
-# This file is part of The Agile Deployment Toolkit.
-# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################################
-################################################################################################
-#set -x
-#!/bin/sh
 ######################################################################################################
 # Description: This script will install the Vultr CLI toolkit
 # Author: Peter Winter
@@ -39,7 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
-set -x
+#set -x
 
 if ( [ "${1}" != "" ] )
 then
@@ -49,7 +28,7 @@ fi
 if ( [ "${buildos}" = "ubuntu" ] )
 then 
         vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"
-        /usr/bin/wget -qO- https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz | /usr/bin/tar xfz - -C /usr/bin
+        /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd63.tar.gz -O - | /usr/bin/tar -xz -C /usr/bin
         /bin/mv /usr/bin/vultr-cli /usr/bin/vultr
         /bin/chown root:root /usr/bin/vultr
 #       if ( [ ! -f /usr/bin/make ] )
@@ -77,7 +56,7 @@ fi
 if ( [ "${buildos}" = "debian" ] )
 then
         vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"
-        /usr/bin/wget -qO- https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz | /usr/bin/tar xfz - -C /usr/bin
+        /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd63.tar.gz -O - | /usr/bin/tar -xz -C /usr/bin
         /bin/mv /usr/bin/vultr-cli /usr/bin/vultr
         /bin/chown root:root /usr/bin/vultr
 
