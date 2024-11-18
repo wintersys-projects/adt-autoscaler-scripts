658e2273705c92c840ee8df30681352e09d7a7e0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-18 12:45:52 +0000

    Update MonitorFirewall.sh

--- security/MonitorFirewall.sh
+++ security/MonitorFirewall.sh
@@ -50,7 +50,7 @@ if ( [ "${firewall}" = "ufw" ] )
 then
 	if ( [ "`/usr/sbin/ufw status | /bin/grep inactive`" != "" ] )
 	then
-		${HOME}/providerscripts/email/SendEmail.sh "FIREWALL INACTIVE" "Just so you know, your firewall is inactive on machine `${HOME}/providerscripts/utilities/GetPublicIP.sh`. The machine may still be initialsing after a reboot, which can take some minutes, but if these messages continue indefinitely, then you need to look into why the firewall is inactive." "ERROR"
+		${HOME}/providerscripts/email/SendEmail.sh "FIREWALL (ufw) INACTIVE" "Just so you know, your firewall is inactive on machine `${HOME}/providerscripts/utilities/GetPublicIP.sh`. The machine may still be initialsing after a reboot, which can take some minutes, but if these messages continue indefinitely, then you need to look into why the firewall is inactive." "ERROR"
 		/bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
 	fi
 elif ( [ "${firewall}" = "iptables" ] )
@@ -59,7 +59,7 @@ then
  	then
   		if ( [ "`${HOME}/providerscripts/utilities/RunServiceCommand.sh netfilter-persistent status | /bin/grep active`" = "" ] )
 		then
-  			${HOME}/providerscripts/email/SendEmail.sh "FIREWALL INACTIVE" "Just so you know, your firewall is inactive on machine `${HOME}/providerscripts/utilities/GetPublicIP.sh`. The machine may still be initialsing after a reboot, which can take some minutes, but if these messages continue indefinitely, then you need to look into why the firewall is inactive." "ERROR"
+  			${HOME}/providerscripts/email/SendEmail.sh "FIREWALL (iptables) INACTIVE" "Just so you know, your firewall is inactive on machine `${HOME}/providerscripts/utilities/GetPublicIP.sh`. The machine may still be initialsing after a reboot, which can take some minutes, but if these messages continue indefinitely, then you need to look into why the firewall is inactive." "ERROR"
 			/bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
    		fi
 	fi
