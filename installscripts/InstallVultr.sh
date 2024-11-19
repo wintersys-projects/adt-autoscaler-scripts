503bf1b6c4d6170017effef968ad929ac7196d00
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-19 18:20:25 +0000

    Update InstallVultr.sh

--- installscripts/InstallVultr.sh
+++ installscripts/InstallVultr.sh
@@ -27,10 +27,10 @@ fi
 
 if ( [ "${buildos}" = "ubuntu" ] )
 then 
-        vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"
-        /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz -O - | /usr/bin/tar -xz -C /usr/bin
-        /bin/mv /usr/bin/vultr-cli /usr/bin/vultr
-        /bin/chown root:root /usr/bin/vultr
+        vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"        #####UBUNTU-VULTR-BINARY#####
+        /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz -O - | /usr/bin/tar -xz -C /usr/bin   #####UBUNTU-VULTR-BINARY#####
+        /bin/mv /usr/bin/vultr-cli /usr/bin/vultr                         #####UBUNTU-VULTR-BINARY#####
+        /bin/chown root:root /usr/bin/vultr                                #####UBUNTU-VULTR-BINARY#####
 #       if ( [ ! -f /usr/bin/make ] )
 #       then
 #               DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq install make
@@ -55,10 +55,10 @@ fi
 
 if ( [ "${buildos}" = "debian" ] )
 then
-        vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"
-        /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz -O - | /usr/bin/tar -xz -C /usr/bin
-        /bin/mv /usr/bin/vultr-cli /usr/bin/vultr
-        /bin/chown root:root /usr/bin/vultr
+        vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"        #####DEBIAN-VULTR-BINARY#####
+        /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz -O - | /usr/bin/tar -xz -C /usr/bin #####DEBIAN-VULTR-BINARY####
+        /bin/mv /usr/bin/vultr-cli /usr/bin/vultr        #####DEBIAN-VULTR-BINARY####
+        /bin/chown root:root /usr/bin/vultr              #####DEBIAN-VULTR-BINARY####
 
  #      if ( [ ! -f /usr/bin/make ] )
 #       then
