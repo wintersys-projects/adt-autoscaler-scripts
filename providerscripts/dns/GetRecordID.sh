81057de5a6ba787e6dfafbc63698d181fa075c84
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-21 22:42:30 +0000

    Update GetRecordID.sh

--- providerscripts/dns/GetRecordID.sh
+++ providerscripts/dns/GetRecordID.sh
@@ -87,6 +87,5 @@ if ( [ "${dns}" = "vultr" ] )
 then
 	HOME="`/bin/cat /home/homedir.dat`"
 	export VULTR_API_KEY="`/bin/ls ${HOME}/.config/VULTRAPIKEY:* | /usr/bin/awk -F':' '{print $NF}'`"
-	/usr/bin/vultr dns record list ${domainurl} | /bin/grep ${ip} | /usr/bin/awk '{print $1}'
-
+	/usr/bin/vultr dns record list ${domainurl} -o json | /usr/bin/jq -r '.records[] | select (.data == "'${ip}'").id'
 fi
