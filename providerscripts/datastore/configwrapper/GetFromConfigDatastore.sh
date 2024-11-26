cde28e14d2895657b73e98cd8b945b64a69491b1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-26 16:53:33 +0000

    Update GetFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh
@@ -22,8 +22,7 @@
 
 export HOME=`/bin/cat /home/homedir.dat`
 WEBSITE_URL="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'WEBSITEURL'`"
-configbucket="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
-configbucket="${configbucket}-config"
+configbucket="${WEBSITE_URL}-config"
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
