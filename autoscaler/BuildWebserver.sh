5212cb22f31268692d8ad61d9093d49adc96f383
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-03 13:13:47 +0000

    Update BuildWebserver.sh

--- autoscaler/BuildWebserver.sh
+++ autoscaler/BuildWebserver.sh
@@ -195,6 +195,7 @@ name="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{print $1}'`"
 
 # Set up the webservers properties, like its name and so on.
 rnd="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1`"
+rnd="${rnd}-`/usr/bin/openssl rand -base64 32 | /usr/bin/tr -cd 'a-zA-Z0-9' | /usr/bin/cut -b 1-4`"
 server_type="ws-${REGION}-${BUILD_IDENTIFIER}"
 autoscalerip="`${HOME}/providerscripts/utilities/GetPublicIP.sh`"
 autoscaler_name="`${HOME}/providerscripts/server/GetServerName.sh ${autoscalerip} ${CLOUDHOST}`"
