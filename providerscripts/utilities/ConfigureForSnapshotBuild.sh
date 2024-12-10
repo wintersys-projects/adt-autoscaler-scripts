3f2b24f583c70547afc4324dec5ceefadd1cb179
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-10 20:20:02 +0000

    Update ConfigureForSnapshotBuild.sh

--- providerscripts/utilities/ConfigureForSnapshotBuild.sh
+++ providerscripts/utilities/ConfigureForSnapshotBuild.sh
@@ -1,4 +1,5 @@
-if ( [ -f ${HOME}/runtime/AUTOSCALER_READY ] && [ ! -f ${HOME}/runtime/SNAPSHOT_PRIMED ] )
+if ( [ -f ${HOME}/runtime/AUTOSCALER_READY ] && [ -f ${HOME}/runtime/SNAPSHOT_BUILT ] && [ ! -f ${HOME}/runtime/SNAPSHOT_PRIMED ] )
 then
  	${HOME}/providerscripts/utilities/UpdateInfrastructure.sh
+  /bin/touch ${HOME}/runtime/SNAPSHOT_PRIMED
 fi
