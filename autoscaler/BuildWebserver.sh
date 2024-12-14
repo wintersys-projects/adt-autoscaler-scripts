1d13cdb8525ef1e551d442f6599f5f7f13130aa1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-14 11:13:44 +0000

    Update BuildWebserver.sh

--- autoscaler/BuildWebserver.sh
+++ autoscaler/BuildWebserver.sh
@@ -378,7 +378,7 @@ then
 		count="0"
 		/bin/echo "${0} `/bin/date`: Performing mount checks for ip address ${ip}" >> ${HOME}/logs/${logdir}/MonitoringWebserverBuildLog.log
 		/usr/bin/ssh -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${private_ip} "${CUSTOM_USER_SUDO} ${HOME}/providerscripts/datastore/SetupAssetsStore.sh"
-		while ( [ "${count}" -lt "10" ] &&  [ "`/usr/bin/ssh -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${private_ip} "${CUSTOM_USER_SUDO} ${HOME}/providerscripts/utilities/AreAssetsMounted.sh"`" != "MOUNTED" ] )
+		while ( [ "${count}" -lt "10" ] &&  [ "`/usr/bin/ssh -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${private_ip} "${CUSTOM_USER_SUDO} ${HOME}/providerscripts/utilities/status/AreAssetsMounted.sh"`" != "MOUNTED" ] )
 		do
 			counts="`/usr/bin/expr ${count} + 1`"
 			/bin/sleep 10
