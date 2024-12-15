699704d0f0f37aed8098f672dc014fe00359e332
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-15 12:44:40 +0000

    Update as.sh

--- as.sh
+++ as.sh
@@ -172,7 +172,7 @@ ClientAliveInterval 200
 ClientAliveCountMax 10" >> /etc/ssh/sshd_config
 fi
 
-${HOME}/providerscripts/utilities/RunServiceCommand.sh ssh restart
+${HOME}/providerscripts/utilities/processing/RunServiceCommand.sh ssh restart
 
 #Install the programs that we need to use when building the autoscaler
 
