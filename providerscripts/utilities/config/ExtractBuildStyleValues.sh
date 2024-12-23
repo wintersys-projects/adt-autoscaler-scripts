1be7f2a30657fd41ad34e86c36469486524106c0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-23 17:13:20 +0000

    Update ExtractBuildStyleValues.sh

--- providerscripts/utilities/config/ExtractBuildStyleValues.sh
+++ providerscripts/utilities/config/ExtractBuildStyleValues.sh
@@ -25,8 +25,8 @@ export HOME="`/bin/cat /home/homedir.dat`"
 
 if ( [ "${1}" != "" ] && [ "${2}" = "stripped" ] )
 then
-	/bin/grep -a "^${1}:" ${HOME}/.ssh/buildstyles.dat | /usr/bin/awk -F':' '{$1=""; print $0}' | /bin/sed 's/^ //g' | /bin/sed 's/ $//g'
+	/bin/grep -a "^${1}:" ${HOME}/runtime/buildstyles.dat | /usr/bin/awk -F':' '{$1=""; print $0}' | /bin/sed 's/^ //g' | /bin/sed 's/ $//g'
 elif ( [ "${1}" != "" ] && [ "${2}" != "stripped" ] )
 then 
-	/bin/grep -a "^${1}:" ${HOME}/.ssh/buildstyles.dat
+	/bin/grep -a "^${1}:" ${HOME}/runtime/buildstyles.dat
 fi
