7e08b94b348d38ba6df8ce6ff3b7657ca7468812
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-23 17:09:57 +0000

    Update StoreConfigValue.sh

--- providerscripts/utilities/config/StoreConfigValue.sh
+++ providerscripts/utilities/config/StoreConfigValue.sh
@@ -31,11 +31,11 @@ fi
 if ( [ "${1}" != "" ] && [ "${2}" != "" ] )
 then
    # /bin/sed -i "/.*${1}:/d" ${HOME}/.ssh/autoscaler_configuration_settings.dat
-	/bin/sed -i "/^${1}:/d" ${HOME}/.ssh/autoscaler_configuration_settings.dat
-	/bin/echo "${1}:${2}" >> ${HOME}/.ssh/autoscaler_configuration_settings.dat
+	/bin/sed -i "/^${1}:/d" ${HOME}/runtime/autoscaler_configuration_settings.dat
+	/bin/echo "${1}:${2}" >> ${HOME}/runtime/autoscaler_configuration_settings.dat
 elif ( [ "${1}" != "" ] && [ "${2}" = "" ] )
 then
-	/bin/sed -i "/^${1}$/d" ${HOME}/.ssh/autoscaler_configuration_settings.dat
+	/bin/sed -i "/^${1}$/d" ${HOME}/runtime/autoscaler_configuration_settings.dat
    # /bin/sed -i "/.*${1}$/d" ${HOME}/.ssh/autoscaler_configuration_settings.dat
-	/bin/echo "${1}" >> ${HOME}/.ssh/autoscaler_configuration_settings.dat
+	/bin/echo "${1}" >> ${HOME}/runtime/autoscaler_configuration_settings.dat
 fi
