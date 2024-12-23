fbcdca0ce84593b293fc88b8c9caac3143150251
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-23 20:09:09 +0000

    Update as.sh

--- as.sh
+++ as.sh
@@ -87,7 +87,7 @@ if ( [ -f ${HOME}/.ssh/autoscaler_configuration_settings.dat ] )
 then
 	/bin/cp ${HOME}/.ssh/autoscaler_configuration_settings.dat ${HOME}/runtime/autoscaler_configuration_settings.dat
  	/bin/chown root:root ${HOME}/runtime/autoscaler_configuration_settings.dat
- 	/bin/chmod 400 ${HOME}/runtime/autoscaler_configuration_settings.dat
+ 	/bin/chmod 640 ${HOME}/runtime/autoscaler_configuration_settings.dat
   	/bin/mv ${HOME}/.ssh/autoscaler_configuration_settings.dat ${HOME}/.ssh/autoscaler_configuration_settings.dat.original
 fi
 
@@ -103,7 +103,7 @@ if ( [ -f ${HOME}/.ssh/buildstyles.dat ] )
 then
 	/bin/cp ${HOME}/.ssh/buildstyles.dat ${HOME}/runtime/buildstyles.dat
  	/bin/chown root:root ${HOME}/runtime/buildstyles.dat
- 	/bin/chmod 540 ${HOME}/runtime/buildstyles.dat
+ 	/bin/chmod 640 ${HOME}/runtime/buildstyles.dat
   	/bin/mv ${HOME}/.ssh/buildstyles.dat ${HOME}/.ssh/buildstyles.dat.original
 fi
 
