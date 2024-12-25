88dae2a3fb8df65c54601b1bb19cc2d1ed985e26
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-25 20:07:23 +0000

    Create InstallS3CMD.sh

---
+++ installscripts/InstallS3CMD.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+###################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: Install the s3cmd tool for manipulating the Datastores
+####################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#####################################################################################
+#####################################################################################
+#set -x
+
+BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+
+apt=""
+if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
+then
+	apt="/usr/bin/apt-get"
+elif ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
+then
+	apt="/usr/sbin/apt-fast"
+fi
+
+if ( [ "${apt}" != "" ] )
+then
+	if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
+ 	then
+		if ( [ "${BUILDOS}" = "ubuntu" ] )
+		then
+			DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install s3cmd	#####UBUNTU-S3CMD-REPO#####
+		fi
+
+		if ( [ "${BUILDOS}" = "debian" ] )
+		then
+			DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install s3cmd	#####DEBIAN-S3CMD-REPO#####
+		fi
+  	fi
+    	/bin/touch ${HOME}/runtime/installedsoftware/InstallS3CMD.sh				
+fi
+
+if ( [ -f ${HOME}/.s3cfg ] )
+then
+	/bin/cp ${HOME}/.s3cfg /root
+fi
