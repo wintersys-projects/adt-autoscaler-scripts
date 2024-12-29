36b4901eb89415efcefe220f2a945b52fbf27b18
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-29 11:01:02 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -9,41 +9,6 @@ BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDO
 >&2 /bin/echo "${0} UpdateAndUpgrade.sh"
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
 
-if ( [ "${1}" = "preinstall" ] )
-then
-  scripts="`/bin/cat ${HOME}/installscripts/InstallCoreSoftware.sh | /bin/grep BUILDOS | /bin/grep -v "Up.*" | /usr/bin/awk '{print $1}'`"
-  
-  package_names=""
-
-  for script in ${scripts}
-  do
-        script="`/bin/echo ${script} | /bin/sed -e 's,\${HOME},'${HOME}',g'`"
-        package_names="${package_names} `/bin/cat ${script} | /bin/grep DEBIAN_FRONTEND | /usr/bin/awk '{print $8}' | /usr/bin/sort -u | /usr/bin/uniq | /usr/bin/tr '\n' ' '`"
-  done
-
-  apt=""
-  if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
-  then
-        apt="/usr/bin/apt-get"
-  elif ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
-  then
-        apt="/usr/sbin/apt-fast"
-  fi
-
-  if ( [ "${apt}" != "" ] )
-  then
-        if ( [ "${BUILDOS}" = "ubuntu" ] )
-        then
-                DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install ${package_names}
-        fi
-        if ( [ "${BUILDOS}" = "debian" ] )
-        then
-                DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install ${package_names}
-        fi
-  fi
-fi
-#Install the programs that we need to use when building the autoscaler
->&2 /bin/echo "${0} Installing software packages "
 
 >&2 /bin/echo "${0} InstallGo.sh"
 ${HOME}/installscripts/InstallGo.sh ${BUILDOS} 
