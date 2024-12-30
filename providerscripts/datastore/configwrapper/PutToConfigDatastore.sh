1353634352e1422ad2af1a1103509c1a7be0d362
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-30 13:25:51 +0000

    Update PutToConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PutToConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PutToConfigDatastore.sh
@@ -20,29 +20,38 @@
 ######################################################################################
 #set -x
 
-BUILD_HOME="`/bin/cat /home/buildhome.dat`"
-CLOUDHOST="`/bin/cat ${BUILD_HOME}/runtimedata/BUILD_MACHINE_CLOUDHOST`"
-BUILD_IDENTIFIER="`/bin/cat ${BUILD_HOME}/runtimedata/ACTIVE_BUILD_IDENTIFIER`"
-SERVER_USER="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
-TOKEN="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
-WEBSITE_URL="`${BUILD_HOME}/helperscripts/GetVariableValue.sh WEBSITE_URL`"
+file_to_put="${1}"
+place_to_put="${2}"
 
+export HOME=`/bin/cat /home/homedir.dat`
+
+WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+SERVER_USER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
+TOKEN="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
 configbucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${TOKEN}"
 
-if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "s3cmd" ] )
+
+if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
         datastore_tool="/usr/bin/s3cmd put "
-elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "s5cmd" ] )
+elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
         host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
         datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} cp "
 fi
 
-if ( [ "${2}" != "" ] )
+if ( [ ! -f ${file_to_put} ] )
 then
-        command="${datastore_tool} $1 s3://${configbucket}/$2"
+        /bin/touch /tmp/${file_to_put}
+        file_to_put="/tmp/${file_to_put}"
+fi
+        
+
+if ( [ "${place_to_put}" != "" ] )
+then
+        command="${datastore_tool} ${file_to_put} s3://${configbucket}/${place_to_put}"
 else
-        command="${datastore_tool} $1 s3://${configbucket}"
+        command="${datastore_tool} ${file_to_put} s3://${configbucket}"
 fi
 
 count="0"
@@ -59,3 +68,8 @@ do
                 satisfied="1"
         fi
 done 
+
+if ( [ -f /tmp/${file_to_put} ] )
+then
+        /bin/rm /tmp/${file_to_put}
+fi
