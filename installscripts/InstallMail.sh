8c96fe6b43b0ebd7d1d09d2b302013e3228f8082
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 09:40:04 +0000

    Update InstallMail.sh

--- installscripts/InstallMail.sh
+++ installscripts/InstallMail.sh
@@ -1,1 +1,48 @@
-apt install mailutils -y
+#!/bin/sh
+###############################################################################################
+# Description: This script will install sendemail
+# Author: Peter Winter
+# Date: 12/01/2017
+###############################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################################
+################################################################################################
+
+if ( [ "${1}" != "" ] )
+then
+	buildos="${1}"
+fi
+
+apt=""
+if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
+then
+	apt="/usr/bin/apt-get"
+elif ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
+then
+	apt="/usr/sbin/apt-fast"
+fi
+
+if ( [ "${apt}" != "" ] )
+then
+	if ( [ "${buildos}" = "ubuntu" ] )
+	then
+		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install mailutils		#####UBUNTU-SENDEMAIL-REPO#####
+	fi
+
+	if ( [ "${buildos}" = "debian" ] )
+	then
+		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install mailutils		#####DEBIAN-SENDEMAIL-REPO#####
+	fi
+     	/bin/touch ${HOME}/runtime/installedsoftware/InstallMail.sh	
+fi
