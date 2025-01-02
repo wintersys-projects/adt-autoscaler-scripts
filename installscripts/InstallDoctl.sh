e97167cb802b42c6dfaa0715a739db3fd1b8fcdb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-02 09:46:28 +0000

    Update InstallDoctl.sh

--- installscripts/InstallDoctl.sh
+++ installscripts/InstallDoctl.sh
@@ -24,26 +24,29 @@ then
 	buildos="${1}"
 fi
 
+export DEBIAN_FRONTEND=noninteractive
+install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
+
 if ( [ "${buildos}" = "ubuntu" ] )
 then
-	 DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq -y install snapd	#####UBUNTU-DOCTL-REPO#####
-	 snap="`/usr/bin/whereis snap | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/awk '{print $1}'`"		#####UBUNTU-DOCTL-REPO-SKIP#####
-	 ${snap} install doctl											#####UBUNTU-DOCTL-REPO-SKIP#####
-	 /usr/bin/ln -s /snap/bin/doctl /usr/local/bin/doctl							#####UBUNTU-DOCTL-REPO#####
-	 /bin/mkdir -p /root/.config/doctl 									#####UBUNTU-DOCTL-REPO#####
-	 /bin/cp ${HOME}/.config/doctl/config.yaml /root/.config/doctl						#####UBUNTU-DOCTL-REPO#####
-	 /bin/chmod 400 ${HOME}/.config/doctl/config.yaml /root/.config/doctl/config.yaml			#####UBUNTU-DOCTL-REPO#####
+	 ${install_command} snapd	
+	 snap="`/usr/bin/whereis snap | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/awk '{print $1}'`"		
+	 ${snap} install doctl											
+	 /usr/bin/ln -s /snap/bin/doctl /usr/local/bin/doctl							
+	 /bin/mkdir -p /root/.config/doctl 									
+	 /bin/cp ${HOME}/.config/doctl/config.yaml /root/.config/doctl						
+	 /bin/chmod 400 ${HOME}/.config/doctl/config.yaml /root/.config/doctl/config.yaml			
 fi
 
 if ( [ "${buildos}" = "debian" ] )
 then
-	 DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq -y install snapd	#####DEBIAN-DOCTL-REPO#####
-	 snap="`/usr/bin/whereis snap | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/awk '{print $1}'`"		#####DEBIAN-DOCTL-REPO-SKIP#####
-	 ${snap} install doctl											#####DEBIAN-DOCTL-REPO-SKIP#####
-	 /usr/bin/ln -s /snap/bin/doctl /usr/local/bin/doctl							#####DEBIAN-DOCTL-REPO#####
-	 /bin/mkdir -p /root/.config/doctl 									#####DEBIAN-DOCTL-REPO#####
-	 /bin/cp ${HOME}/.config/doctl/config.yaml /root/.config/doctl						#####DEBIAN-DOCTL-REPO#####
-	 /bin/chmod 400 ${HOME}/.config/doctl/config.yaml /root/.config/doctl/config.yaml			#####DEBIAN-DOCTL-REPO#####
+	 ${install_command} snapd	
+	 snap="`/usr/bin/whereis snap | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/awk '{print $1}'`"		
+	 ${snap} install doctl											
+	 /usr/bin/ln -s /snap/bin/doctl /usr/local/bin/doctl							
+	 /bin/mkdir -p /root/.config/doctl 									
+	 /bin/cp ${HOME}/.config/doctl/config.yaml /root/.config/doctl						
+	 /bin/chmod 400 ${HOME}/.config/doctl/config.yaml /root/.config/doctl/config.yaml			
 fi
 /bin/touch ${HOME}/runtime/installedsoftware/InstallDoctl.sh	
 
