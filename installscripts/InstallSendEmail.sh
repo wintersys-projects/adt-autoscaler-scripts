9a9b2610649ada33f5c1233220e4597220e8af41
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-02 10:03:13 +0000

    Update InstallSendEmail.sh

--- installscripts/InstallSendEmail.sh
+++ installscripts/InstallSendEmail.sh
@@ -33,16 +33,19 @@ then
 	apt="/usr/sbin/apt-fast"
 fi
 
+export DEBIAN_FRONTEND=noninteractive
+install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
+
 if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install sendemail		#####UBUNTU-SENDEMAIL-REPO#####
+		${install_command} sendemail		
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install sendemail		#####DEBIAN-SENDEMAIL-REPO#####
+		${install_command} sendemail		
 	fi
      	/bin/touch ${HOME}/runtime/installedsoftware/InstallSendEmail.sh	
 fi
