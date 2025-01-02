3dfbbed07afc61324bf4327c89cf8e2cb13ebbe5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-02 13:45:13 +0000

    Update BuildWebserver.sh

--- autoscaler/BuildWebserver.sh
+++ autoscaler/BuildWebserver.sh
@@ -368,7 +368,7 @@ done
 
 
 /bin/echo "${0} `/bin/date`: Checking webserver is up" >> ${HOME}/logs/${logdir}/MonitoringWebserverBuildLog.log
-/usr/bin/ssh -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${private_ip}  "/bin/ls /home/${SERVER_USER}/providerscripts/webserver/CheckWebserverIsUp.sh"`"
+/usr/bin/ssh -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${private_ip}  "/bin/ls /home/${SERVER_USER}/providerscripts/webserver/CheckWebserverIsUp.sh"
 
 #Do some checks to make sure the machine has come online and so on
 count="0"
