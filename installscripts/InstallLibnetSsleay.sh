2f4403551dd2a54051c5f866ae8886f9e108cf93
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-02 09:52:11 +0000

    Update InstallLibnetSsleay.sh

--- installscripts/InstallLibnetSsleay.sh
+++ installscripts/InstallLibnetSsleay.sh
@@ -33,16 +33,19 @@ then
 	apt="/usr/sbin/apt-fast"
 fi
 
+export DEBIAN_FRONTEND=noninteractive
+install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
+
 if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install libnet-ssleay-perl #####UBUNTU-LIBNETSSL-REPO#####
+		${install_command} libnet-ssleay-perl 
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install libnet-ssleay-perl	 #####DEBIAN-LIBNETSSL-REPO#####
+		${install_command} libnet-ssleay-perl 
 	fi
    	 /bin/touch ${HOME}/runtime/installedsoftware/InstallLibnetSsleay.sh	
 fi
