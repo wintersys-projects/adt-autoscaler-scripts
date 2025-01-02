57ea4a4a20a036edf8eea5476a7bedc7f35b6a84
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-02 09:54:56 +0000

    Update InstallMail.sh

--- installscripts/InstallMail.sh
+++ installscripts/InstallMail.sh
@@ -33,16 +33,19 @@ then
 	apt="/usr/sbin/apt-fast"
 fi
 
+export DEBIAN_FRONTEND=noninteractive
+install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
+
 if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install mailutils		#####UBUNTU-SENDEMAIL-REPO#####
+		${install_command} mailutils		
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install mailutils		#####DEBIAN-SENDEMAIL-REPO#####
+		${install_command} mailutils		
 	fi
      	/bin/touch ${HOME}/runtime/installedsoftware/InstallMail.sh	
 fi
