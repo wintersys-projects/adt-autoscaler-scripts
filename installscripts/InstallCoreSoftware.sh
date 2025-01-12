8b41fba340d440870d674a65179f748708131c8a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-12 23:08:45 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -8,6 +8,8 @@ BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDO
 
 >&2 /bin/echo "${0} UpdateAndUpgrade.sh"
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallNetworkManager.sh"
+${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallFirewall.sh"
 ${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} 
 >&2 /bin/echo "${0} Installing Datastore tools"
@@ -15,28 +17,27 @@ ${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS}
 
 >&2 /bin/echo "${0} InstallGo.sh"
 ${HOME}/installscripts/InstallGo.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallCurl.sh"
-${HOME}/installscripts/InstallCurl.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallSSHPass.sh"
-${HOME}/installscripts/InstallSSHPass.sh ${BUILDOS} 
+#>&2 /bin/echo "${0} InstallCurl.sh"
+#${HOME}/installscripts/InstallCurl.sh ${BUILDOS} 
+#>&2 /bin/echo "${0} InstallSSHPass.sh"
+#${HOME}/installscripts/InstallSSHPass.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallBC.sh"
 ${HOME}/installscripts/InstallBC.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallNetworkManager.sh"
-${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallJQ.sh"
-${HOME}/installscripts/InstallJQ.sh ${BUILDOS} 
+
+#>&2 /bin/echo "${0} InstallJQ.sh"
+#${HOME}/installscripts/InstallJQ.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallEmailUtil.sh"
 ${HOME}/installscripts/InstallEmailUtil.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallLibioSocket.sh"
-${HOME}/installscripts/InstallLibioSocket.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallLibnetSsleay.sh"
-${HOME}/installscripts/InstallLibnetSsleay.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallSysStat.sh"
-${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallRsync.sh"
-${HOME}/installscripts/InstallRsync.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallCron.sh"
-${HOME}/installscripts/InstallCron.sh ${BUILDOS} 
+#>&2 /bin/echo "${0} InstallLibioSocket.sh"
+#${HOME}/installscripts/InstallLibioSocket.sh ${BUILDOS} 
+#>&2 /bin/echo "${0} InstallLibnetSsleay.sh"
+#${HOME}/installscripts/InstallLibnetSsleay.sh ${BUILDOS} 
+#>&2 /bin/echo "${0} InstallSysStat.sh"
+#${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} 
+#>&2 /bin/echo "${0} InstallRsync.sh"
+#${HOME}/installscripts/InstallRsync.sh ${BUILDOS} 
+#>&2 /bin/echo "${0} InstallCron.sh"
+#${HOME}/installscripts/InstallCron.sh ${BUILDOS} 
 >&2 /bin/echo "${0} Install Monitoring Gear"
 ${HOME}/installscripts/InstallMonitoringGear.sh 
 >&2 /bin/echo "${0} Installing cloudtools"
