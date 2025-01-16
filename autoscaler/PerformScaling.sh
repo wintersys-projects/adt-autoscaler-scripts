965b56b587c734f20792d88007ccadd55b8a98c9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-16 20:22:02 +0000

    Update PerformScaling.sh

--- autoscaler/PerformScaling.sh
+++ autoscaler/PerformScaling.sh
@@ -39,7 +39,7 @@ then
 	exit
 fi
 
-if ( [ ! -f ${HOME}/runtime/INITIAL_SCALING_PROCESSED ] )
+if ( [ -f ${HOME}/runtime/INITIAL_BUILD_COMPLETED ] )
 then
    /bin/echo "${0} `/bin/date`: Initial scaling process has completed and I am authorising scaling" >> ${HOME}/logs/${logdir}/ScalingEventsLog.log
    /bin/touch ${HOME}/runtime/AUTHORISED_TO_SCALE  
@@ -65,14 +65,6 @@ fi
 #	fi
 #fi
 
-if ( [ ! -f ${HOME}/runtime/INITIAL_SCALING_PROCESSED ] )
-then
-	/bin/touch ${HOME}/runtime/AUTHORISED_TO_SCALE  
-	if ( [ -f ${HOME}/runtime/NOT_AUTHORISED_TO_SCALE ] )
-	then
-		/bin/rm ${HOME}/runtime/NOT_AUTHORISED_TO_SCALE
-	fi
-fi
 
 if ( [ -f ${HOME}/runtime/NOT_AUTHORISED_TO_SCALE ] )
 then
