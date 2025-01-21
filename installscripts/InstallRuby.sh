c44f2fa272918006e777e97869818af199286e70
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-21 11:31:41 +0000

    Update InstallRuby.sh

--- installscripts/InstallRuby.sh
+++ installscripts/InstallRuby.sh
@@ -40,12 +40,12 @@ if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		${install_command} ruby	
+		eval ${install_command} ruby	
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		${install_command} ruby	
+		eval ${install_command} ruby	
 	fi
    	/bin/touch ${HOME}/runtime/installedsoftware/InstallRuby.sh	
 fi
