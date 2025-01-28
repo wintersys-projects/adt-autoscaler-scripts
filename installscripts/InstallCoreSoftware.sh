c94871914f54405a3582f76e1672d32dcfd6a5a5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-28 22:58:47 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -9,8 +9,8 @@ BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDO
 #>&2 /bin/echo "${0} UpdateAndUpgrade.sh"
 #${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
 
-/bin/sed -i "s/mirrors.linode.com/mirror.katapult.io/g" /etc/apt/sources.list
-/bin/sed -i "s/mirrors.digitalocean.com/mirror.katapult.io/g" /etc/apt/mirrors/debian.list
+#/bin/sed -i "s/mirrors.linode.com/mirror.katapult.io/g" /etc/apt/sources.list
+#/bin/sed -i "s/mirrors.digitalocean.com/mirror.katapult.io/g" /etc/apt/mirrors/debian.list
 
 >&2 /bin/echo "${0} Update.sh"
 ${HOME}/installscripts/Update.sh ${BUILDOS}
