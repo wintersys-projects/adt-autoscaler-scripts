131869699f6cb84dc560f2f10f97047c3aadda6b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-01 15:23:14 +0000

    Update Upgrade.sh

--- installscripts/Upgrade.sh
+++ installscripts/Upgrade.sh
@@ -20,11 +20,18 @@
 ####################################################################################
 #set -x
 
-#if ( [ "${1}" != "" ] )
-#then
-#	buildos="${1}"
-#fi
-BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+if ( [ "${1}" != "" ] )
+then
+    buildos="${1}"
+fi
+
+if ( [ "${buildos}" = "" ] )
+then
+    BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+else 
+    BUILDOS="${buildos}"
+fi
+
 apt=""
 if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
