0a3e810d6883ab8363b22760b5c74d304b6e629a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-01 15:14:31 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -4,6 +4,18 @@ then
   /bin/mkdir -p ${HOME}/runtime/installedsoftware
 fi
 
+if ( [ "${1}" != "" ] )
+then
+    buildos="${1}"
+fi
+
+if ( [ "${buildos}" = "" ] )
+then
+    BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+else 
+    BUILDOS="${buildos}"
+fi
+
 #BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 
 #>&2 /bin/echo "${0} UpdateAndUpgrade.sh"
@@ -13,13 +25,13 @@ fi
 #/bin/sed -i "s/mirrors.digitalocean.com/mirror.katapult.io/g" /etc/apt/mirrors/debian.list
 
 >&2 /bin/echo "${0} InitialUpdate.sh"
-${HOME}/installscripts/InitialUpdate.sh 
+${HOME}/installscripts/InitialUpdate.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallNetworkManager.sh"
-${HOME}/installscripts/InstallNetworkManager.sh  
+${HOME}/installscripts/InstallNetworkManager.sh  ${BUILDOS}
 >&2 /bin/echo "${0} InstallFirewall.sh"
-${HOME}/installscripts/InstallFirewall.sh 
+${HOME}/installscripts/InstallFirewall.sh ${BUILDOS}
 >&2 /bin/echo "${0} Installing Datastore tools"
-${HOME}/installscripts/InstallDatastoreTools.sh 
+${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS}
 
 if ( [ ! -f /usr/bin/s3cmd ] && [ ! -f /usr/bin/s5cmd ] )
 then
@@ -28,17 +40,17 @@ then
 fi
 
 >&2 /bin/echo "${0} InstallJQ.sh"
-${HOME}/installscripts/InstallJQ.sh 
+${HOME}/installscripts/InstallJQ.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallGo.sh"
-${HOME}/installscripts/InstallGo.sh &
+${HOME}/installscripts/InstallGo.sh ${BUILDOS} &
 #>&2 /bin/echo "${0} InstallCurl.sh"
 #${HOME}/installscripts/InstallCurl.sh ${BUILDOS} 
 #>&2 /bin/echo "${0} InstallSSHPass.sh"
 #${HOME}/installscripts/InstallSSHPass.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallBC.sh"
-${HOME}/installscripts/InstallBC.sh 
+${HOME}/installscripts/InstallBC.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallEmailUtil.sh"
-${HOME}/installscripts/InstallEmailUtil.sh 
+${HOME}/installscripts/InstallEmailUtil.sh ${BUILDOS}
 #>&2 /bin/echo "${0} InstallLibioSocket.sh"
 #${HOME}/installscripts/InstallLibioSocket.sh ${BUILDOS} 
 #>&2 /bin/echo "${0} InstallLibnetSsleay.sh"
@@ -46,12 +58,12 @@ ${HOME}/installscripts/InstallEmailUtil.sh
 #>&2 /bin/echo "${0} InstallSysStat.sh"
 #${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallRsync.sh"
-${HOME}/installscripts/InstallRsync.sh
+${HOME}/installscripts/InstallRsync.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallCron.sh"
-${HOME}/installscripts/InstallCron.sh 
+${HOME}/installscripts/InstallCron.sh ${BUILDOS}
 #>&2 /bin/echo "${0} Install Monitoring Gear"
 #${HOME}/installscripts/InstallMonitoringGear.sh 
 >&2 /bin/echo "${0} Installing cloudtools"
-${HOME}/installscripts/InstallCloudhostTools.sh 
+${HOME}/installscripts/InstallCloudhostTools.sh ${BUILDOS}
 
 
