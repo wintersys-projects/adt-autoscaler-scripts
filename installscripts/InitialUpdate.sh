c13d3d9d33600abd27c0142b4e5f76d7d2a8e8b7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-02 10:27:47 +0000

    Update InitialUpdate.sh

--- installscripts/InitialUpdate.sh
+++ installscripts/InitialUpdate.sh
@@ -96,6 +96,7 @@ then
         then
             /bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
         fi
+        /bin/echo "DLLIST=" >> /etc/apt-fast.conf
     fi
     
     if ( [ "${BUILDOS}" = "debian" ] )
@@ -139,6 +140,7 @@ then
         then
             /bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
         fi
+        /bin/echo "DLLIST=" >> /etc/apt-fast.conf
     fi
 fi
 
