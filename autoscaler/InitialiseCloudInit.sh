5e6fd816b19588a10c3f174c7839155b604a7a9a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-05 17:07:14 +0000

    Create InitialiseCloudInit.sh

---
+++ autoscaler/InitialiseCloudInit.sh
@@ -0,0 +1,34 @@
+SERVER_USER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
+SERVER_USER_PASSWORD="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSERPASSWORD'`"
+ALGORITHM="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'ALGORITHM'`"
+BUILD_IDENTIFIER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
+INFRASTRUCTURE_REPOSITORY_OWNER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'INFRASTRUCTUREREPOSITORYOWNER'`"
+SSH_PUBLIC_KEY="`/bin/cat ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER}.pub`"
+SSH_PRIVATE_KEY_TRIMMED="`/bin/cat ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} | /bin/grep -v '^----' | /usr/bin/tr -d '\n'`"
+
+if ( [ ! -d ${HOME}/runtime/cloud-init ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/cloud-init
+fi
+
+if ( [ "${CLOUDHOST}" = "linode" ] )
+then
+        /bin/cp ${HOME}/providerscripts/server/cloud-init/linode.dat ${HOME}/runtime/cloud-init/linode.dat
+fi
+
+
+
+/bin/sed -i "s/XXXXALGORITHMXXXX/${ALGORITHM}/g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s/XXXXSERVER_USERXXXX/${SERVER_USER}/g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s/XXXXSERVER_USER_PASSWORDXXXX/${SERVER_USER_PASSWORD}/g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s/XXXXBUILD_IDENTIFIERXXXX/${BUILD_IDENTIFIER}/g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s/XXXINFRASTRUCTURE_REPOSITORY_OWNERXXXXX/${INFRASTRUCTURE_REPOSITORY_OWNER}/g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s/XXXXSSH_PUBLIC_KEYXXXX/${SSH_PUBLIC_KEY}/g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s/XXXXSSH_PRIVATE_KEYXXXX/${SSH_PRIVATE_KEY_TRIMMED}/g" ${HOME}/runtime/cloud-init/linode.dat
+
+
+
+
+#XXXXWEBSERVER_CONFIGURATIONXXXX
+#XXXXBUILDSTYLES_SETTINGSXXXX
+#XXXXGIT_PROVIDER_DOMAINXXXX
