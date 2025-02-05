8d3c4499ad66a063a9594b81a0b1c214e3e8b090
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-05 17:16:34 +0000

    Update InitialiseCloudInit.sh

--- autoscaler/InitialiseCloudInit.sh
+++ autoscaler/InitialiseCloudInit.sh
@@ -1,3 +1,4 @@
+CLOUDHOST="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
 SERVER_USER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
 SERVER_USER_PASSWORD="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSERPASSWORD'`"
 ALGORITHM="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'ALGORITHM'`"
@@ -6,7 +7,7 @@ INFRASTRUCTURE_REPOSITORY_OWNER="`${HOME}/providerscripts/utilities/config/Extra
 SSH_PUBLIC_KEY="`/bin/cat ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER}.pub`"
 SSH_PRIVATE_KEY_TRIMMED="`/bin/cat ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} | /bin/grep -v '^----' | /usr/bin/tr -d '\n'`"
 webserver_configuration_settings="`/bin/cat ${HOME}/runtime/webserver_configuration_settings.dat | /usr/bin/gzip -f | /usr/bin/base64 | /usr/bin/tr -d '\n'`"
-build_styles_settings="`/bin/cat ${HOME}/runtime/buildstylesscp.dat  | /bin/grep -v "^#" | /usr/bin/gzip -f | /usr/bin/base64 | /usr/bin/tr -d '\n'`"
+build_styles_settings="`/bin/cat ${HOME}/runtime/buildstyles.dat  | /bin/grep -v "^#" | /usr/bin/gzip -f | /usr/bin/base64 | /usr/bin/tr -d '\n'`"
 
 if ( [ ! -d ${HOME}/runtime/cloud-init ] )
 then
@@ -25,8 +26,8 @@ git_provider_domain="github.com"
 /bin/sed -i "s/XXXXSERVER_USER_PASSWORDXXXX/${SERVER_USER_PASSWORD}/g" ${HOME}/runtime/cloud-init/linode.dat
 /bin/sed -i "s/XXXXBUILD_IDENTIFIERXXXX/${BUILD_IDENTIFIER}/g" ${HOME}/runtime/cloud-init/linode.dat
 /bin/sed -i "s/XXXINFRASTRUCTURE_REPOSITORY_OWNERXXXXX/${INFRASTRUCTURE_REPOSITORY_OWNER}/g" ${HOME}/runtime/cloud-init/linode.dat
-/bin/sed -i "s/XXXXSSH_PUBLIC_KEYXXXX/${SSH_PUBLIC_KEY}/g" ${HOME}/runtime/cloud-init/linode.dat
-/bin/sed -i "s/XXXXSSH_PRIVATE_KEYXXXX/${SSH_PRIVATE_KEY_TRIMMED}/g" ${HOME}/runtime/cloud-init/linode.dat
-/bin/sed -i "s/XXXXWEBSERVER_CONFIGURATIONXXXX/${webserver_configuration_settings}/g" ${HOME}/runtime/cloud-init/linode.dat
-/bin/sed -i "s/XXXXBUILDSTYLES_SETTINGSXXXX/${build_styles_settings}/g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s;XXXXSSH_PUBLIC_KEYXXXX;${SSH_PUBLIC_KEY};g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s;XXXXSSH_PRIVATE_KEYXXXX;${SSH_PRIVATE_KEY_TRIMMED};g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s;XXXXWEBSERVER_CONFIGURATIONXXXX;${webserver_configuration_settings};g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s;XXXXBUILDSTYLES_SETTINGSXXXX;${build_styles_settings};g" ${HOME}/runtime/cloud-init/linode.dat
 /bin/sed -i "s/XXXXGIT_PROVIDER_DOMAINXXXX/${git_provider_domain}/g" ${HOME}/runtime/cloud-init/linode.dat
