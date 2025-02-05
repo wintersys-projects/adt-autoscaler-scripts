d1f2b47f5f83c02a1ca7ed15a4497a9f6db47255
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-05 21:02:13 +0000

    Update as.sh

--- as.sh
+++ as.sh
@@ -333,13 +333,13 @@ PRODUCTION="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'PRO
 ${HOME}/providerscripts/utilities/processing/UpdateIPs.sh
 ${HOME}/providerscripts/utilities/housekeeping/CleanupAfterBuild.sh
 
-${HOME}/providerscripts/utilities/security/EnforcePermissions.sh
-
 ${HOME}/providerscripts/email/SendEmail.sh "A NEW AUTOSCALER HAS BEEN SUCCESSFULLY BUILT" "A new autoscaler machine has been built and is now going to reboot before coming available" "INFO"
 
 /bin/touch ${HOME}/runtime/DONT_MESS_WITH_THESE_FILES-SYSTEM_BREAK
 
 /bin/touch ${HOME}/runtime/AUTOSCALER_READY
+
+${HOME}/providerscripts/utilities/security/EnforcePermissions.sh
 
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &
 
