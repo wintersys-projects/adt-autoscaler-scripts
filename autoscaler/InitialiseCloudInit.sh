d6797d501f996bfc7b1a4546e02d40eb4b0cef3e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-06 19:11:53 +0000

    Update InitialiseCloudInit.sh

--- autoscaler/InitialiseCloudInit.sh
+++ autoscaler/InitialiseCloudInit.sh
@@ -24,7 +24,7 @@ git_provider_domain="github.com"
 
 /bin/sed -i "s/XXXXALGORITHMXXXX/${ALGORITHM}/g" ${HOME}/runtime/cloud-init/linode.dat
 /bin/sed -i "s/XXXXSERVER_USERXXXX/${SERVER_USER}/g" ${HOME}/runtime/cloud-init/linode.dat
-/bin/sed -i "s/XXXXSERVER_USER_PASSWORDXXXX/${SERVER_USER_PASSWORD_HASHED}/g" ${HOME}/runtime/cloud-init/linode.dat
+/bin/sed -i "s;XXXXSERVER_USER_PASSWORDXXXX;${SERVER_USER_PASSWORD_HASHED};g" ${HOME}/runtime/cloud-init/linode.dat
 /bin/sed -i "s/XXXXBUILD_IDENTIFIERXXXX/${BUILD_IDENTIFIER}/g" ${HOME}/runtime/cloud-init/linode.dat
 /bin/sed -i "s/XXXXINFRASTRUCTURE_REPOSITORY_OWNERXXXX/${INFRASTRUCTURE_REPOSITORY_OWNER}/g" ${HOME}/runtime/cloud-init/linode.dat
 /bin/sed -i "s;XXXXSSH_PUBLIC_KEYXXXX;${SSH_PUBLIC_KEY};g" ${HOME}/runtime/cloud-init/linode.dat
