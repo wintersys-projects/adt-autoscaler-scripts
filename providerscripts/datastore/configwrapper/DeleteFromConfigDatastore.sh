b83bf5f8cca6740b2b3e36b051df4fa8b3ee8c9f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-18 20:29:55 +0000

    Update DeleteFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh
@@ -38,7 +38,7 @@ then
 fi
 
 count="0"
-while ( [ "`${datastore_tool} s3://${configbucket}/$1 /tmp 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
+while ( [ "`${datastore_tool} s3://${configbucket}/$1 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
 do
         /bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
         /bin/sleep 5
