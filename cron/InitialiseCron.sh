ebc246cb211038d51614c125bd896e5946bcf28f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-19 23:05:04 +0000

    Update InitialiseCron.sh

--- cron/InitialiseCron.sh
+++ cron/InitialiseCron.sh
@@ -59,6 +59,8 @@ if ( [ "${PRODUCTION}" = "1" ] )
 then
 	/bin/echo "*/2 * * * * export HOME="${HOMEDIR}" && ${HOME}/cron/PerformScalingFromCron.sh" >> /var/spool/cron/crontabs/root
 	/bin/echo "*/3 * * * * export HOME="${HOMEDIR}" && ${HOME}/cron/DeadOrAliveFromCron.sh" >> /var/spool/cron/crontabs/root
+ 	#/bin/echo "30 8 * * *  export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/processing/ScalingUpdateEvent.sh 3" >> /var/spool/cron/crontabs/root
+	#/bin/echo "30 17 * * *  export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/processing/ScalingUpdateEvent.sh 3" >> /var/spool/cron/crontabs/root
 fi
 
 /bin/echo "30 3 * * *  export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/housekeeping/RemoveExpiredLogs.sh" >> /var/spool/cron/crontabs/root
