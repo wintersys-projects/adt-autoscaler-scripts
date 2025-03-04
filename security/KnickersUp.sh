5f09df122f77bb1aa56e297957a98805e199aecb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-04 23:53:11 +0000

    Update KnickersUp.sh

--- security/KnickersUp.sh
+++ security/KnickersUp.sh
@@ -43,7 +43,7 @@ then
 	elif ( [ "${firewall}" = "iptables" ] )
  	then
 		#/usr/sbin/iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-    		/usr/sbin/iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
+    		/usr/sbin/iptables -I INPUT -m state --state ESTABLISHED -j ACCEPT
 		/usr/sbin/iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j DROP
 		/usr/sbin/iptables -A INPUT -i lo -j ACCEPT
 		/usr/sbin/iptables -A OUTPUT -o lo -j ACCEPT
