e0ef9a236a816b417355756ada9b47cffd1c0ad9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-22 23:45:22 +0000

    Update InitialiseDatastoreConfig.sh

--- providerscripts/datastore/InitialiseDatastoreConfig.sh
+++ providerscripts/datastore/InitialiseDatastoreConfig.sh
@@ -32,89 +32,87 @@ S3_HOST_BASE="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'S
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-        datastore_tool="/usr/bin/s3cmd"
-        if ( [ -f ${HOME}/.s3cfg ] )
-        then
-	        /bin/rm ${HOME}/.s3cfg
-        fi
+	datastore_tool="/usr/bin/s3cmd"
+	if ( [ -f ${HOME}/.s3cfg ] )
+	then
+		/bin/rm ${HOME}/.s3cfg
+	fi
 
-        /bin/cp ${HOME}/providerscripts/datastore/configfiles/s3-cfg.tmpl ${HOME}/.s3cfg
+	/bin/cp ${HOME}/providerscripts/datastore/configfiles/s3-cfg.tmpl ${HOME}/.s3cfg
 
-        if ( [ "${S3_ACCESS_KEY}" != "" ] )
-        then
-	        /bin/sed -i "s/XXXXACCESSKEYXXXX/${S3_ACCESS_KEY}/" ${HOME}/.s3cfg
-        else
-	        /bin/echo "${0} Couldn't find the S3_ACCESS_KEY setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
-        fi
+	if ( [ "${S3_ACCESS_KEY}" != "" ] )
+	then
+		/bin/sed -i "s/XXXXACCESSKEYXXXX/${S3_ACCESS_KEY}/" ${HOME}/.s3cfg
+	else
+		/bin/echo "${0} Couldn't find the S3_ACCESS_KEY setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
+	fi
 
-        if ( [ "${S3_SECRET_KEY}" != "" ] )
-        then
-	        /bin/sed -i "s/XXXXSECRETKEYXXXX/${S3_SECRET_KEY}/" ${HOME}/.s3cfg
-        else
-	        /bin/echo "${0} Couldn't find the S3_SECRET_KEY setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
-        fi
+	if ( [ "${S3_SECRET_KEY}" != "" ] )
+	then
+		/bin/sed -i "s/XXXXSECRETKEYXXXX/${S3_SECRET_KEY}/" ${HOME}/.s3cfg
+	else
+		/bin/echo "${0} Couldn't find the S3_SECRET_KEY setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
+	fi
 
-        if ( [ "${S3_LOCATION}" != "" ] )
-        then
-	        /bin/sed -i "s/XXXXLOCATIONXXXX/${S3_LOCATION}/" ${HOME}/.s3cfg
-        else
-	        /bin/echo "${0} Couldn't find the S3_LOCATION setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
-        fi
+	if ( [ "${S3_LOCATION}" != "" ] )
+	then
+		/bin/sed -i "s/XXXXLOCATIONXXXX/${S3_LOCATION}/" ${HOME}/.s3cfg
+	else
+		/bin/echo "${0} Couldn't find the S3_LOCATION setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
+	fi
 
-        if ( [ "${S3_HOST_BASE}" != "" ] )
-        then
-	        /bin/sed -i "s/XXXXHOSTBASEXXXX/${S3_HOST_BASE}/" ${HOME}/.s3cfg
-        else
-	        /bin/echo "${0} Couldn't find the S3_HOST_BASE setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
-        fi
+	if ( [ "${S3_HOST_BASE}" != "" ] )
+	then
+		/bin/sed -i "s/XXXXHOSTBASEXXXX/${S3_HOST_BASE}/" ${HOME}/.s3cfg
+	else
+		/bin/echo "${0} Couldn't find the S3_HOST_BASE setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
+	fi
 
-        if ( [ -f /root/.s3cfg ] )
-        then
-	        /bin/rm /root/.s3cfg
-        fi
+	if ( [ -f /root/.s3cfg ] )
+	then
+		/bin/rm /root/.s3cfg
+	fi
 
-        /bin/cp ${HOME}/.s3cfg /root/.s3cfg
-        /bin/chown ${SERVER_USER}:${SERVER_USER} ${HOME}/.s3cfg
-
+	/bin/cp ${HOME}/.s3cfg /root/.s3cfg
+	/bin/chown ${SERVER_USER}:${SERVER_USER} ${HOME}/.s3cfg
 elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ] )
 then
-        datastore_tool="/usr/bin/s5cmd"
-        if ( [ -f ${HOME}/.s5cfg ] )
-        then
-	        /bin/rm ${HOME}/.s5cfg
-        fi
+	datastore_tool="/usr/bin/s5cmd"
+	if ( [ -f ${HOME}/.s5cfg ] )
+	then
+		/bin/rm ${HOME}/.s5cfg
+	fi
 
+	if ( [ "${S3_ACCESS_KEY}" != "" ] )
+	then
+		/bin/echo "[default]" > ${HOME}/.s5cfg 
+ 		/bin/echo "aws_access_key_id = ${S3_ACCESS_KEY}" >> ${HOME}/.s5cfg
+	else
+		/bin/echo "${0} Couldn't find the S3_ACCESS_KEY setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
+	fi
 
-        if ( [ "${S3_ACCESS_KEY}" != "" ] )
-        then
-         	/bin/echo "[default]" > ${HOME}/.s5cfg 
- 	        /bin/echo "aws_access_key_id = ${S3_ACCESS_KEY}" >> ${HOME}/.s5cfg
-        else
-	        /bin/echo "${0} Couldn't find the S3_ACCESS_KEY setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
-        fi
+	if ( [ "${S3_SECRET_KEY}" != "" ] )
+	then
+ 		/bin/echo "aws_secret_access_key = ${S3_SECRET_KEY}" >> ${HOME}/.s5cfg
+	else
+		/bin/echo "${0} Couldn't find the S3_SECRET_KEY setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
+	fi
 
-        if ( [ "${S3_SECRET_KEY}" != "" ] )
-        then
- 	        /bin/echo "aws_secret_access_key = ${S3_SECRET_KEY}" >> ${HOME}/.s5cfg
-        else
-	        /bin/echo "${0} Couldn't find the S3_SECRET_KEY setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
-        fi
+	if ( [ "${S3_HOST_BASE}" != "" ] )
+	then
+		/bin/echo "host_base = ${S3_HOST_BASE}" >> ${HOME}/.s5cfg
+		/bin/echo "alias s5cmd='/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${S3_HOST_BASE}'" >> /root/.bashrc
+	else
+		/bin/echo "${0} Couldn't find the S3_HOST_BASE setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
+	fi
 
-        if ( [ "${S3_HOST_BASE}" != "" ] )
-        then
-  	        /bin/echo "host_base = ${S3_HOST_BASE}" >> ${HOME}/.s5cfg
-    	      /bin/echo "alias s5cmd='/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${S3_HOST_BASE}'" >> /root/.bashrc
-        else
-	        /bin/echo "${0} Couldn't find the S3_HOST_BASE setting" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log  
-        fi
+	if ( [ -f /root/.s5cfg ] )
+	then
+		/bin/rm /root/.s5cfg
+	fi
 
-        if ( [ -f /root/.s5cfg ] )
-        then
-	        /bin/rm /root/.s5cfg
-        fi
-
-        /bin/chown ${SERVER_USER}:${SERVER_USER} ${HOME}/.s5cfg
-        /bin/cp ${HOME}/.s5cfg /root/.s5cfg
+	/bin/chown ${SERVER_USER}:${SERVER_USER} ${HOME}/.s5cfg
+	/bin/cp ${HOME}/.s5cfg /root/.s5cfg
 fi
 
 ${datastore_tool} mb s3://1$$agile 3>&1 2>/dev/null
