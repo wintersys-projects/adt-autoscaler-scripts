3d997ebe544313844ad2f31dfc20b8937e9bdddc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-22 23:38:52 +0000

    Update GetFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh
@@ -30,32 +30,18 @@ configbucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${TOKEN}
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-        datastore_tool="/usr/bin/s3cmd --force get "
+	datastore_tool="/usr/bin/s3cmd --force get "
 elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
-        host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} cp "
+	host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+	datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} cp "
 fi
 
+count="0"
+while ( [ "`${datastore_tool} s3://${configbucket}/$1 $2 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
+do
+	/bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
+	/bin/sleep 5
+	count="`/usr/bin/expr ${count} + 1`"
+done 
 
-#if ( [ "$2" != "" ] )
-#then
-	count="0"
-	while ( [ "`${datastore_tool} s3://${configbucket}/$1 $2 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
-	do
-         	/bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
-        	/bin/sleep 5
-        	count="`/usr/bin/expr ${count} + 1`"
-	done 
-#fi
-
-#if ( [ "$3" = "recursive" ] )
-#then
-#	count="0"#
-#	while ( [ "`${datastore_tool} --force --recursive get s3://${configbucket}/$1 $2 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
-#	do
- #        	/bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
- #       	/bin/sleep 5
- #       	count="`/usr/bin/expr ${count} + 1`"#
-#	done 	 
-#fi
