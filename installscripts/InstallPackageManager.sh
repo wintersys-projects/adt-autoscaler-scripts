775cac68ab4632fa5490c9843509b6ce24de6872
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-22 10:22:07 +0000

    Update InstallPackageManager.sh

--- installscripts/InstallPackageManager.sh
+++ installscripts/InstallPackageManager.sh
@@ -22,103 +22,103 @@ set -x
 
 if ( [ "${1}" != "" ] )
 then
-    buildos="${1}"
+	buildos="${1}"
 fi
 
 if ( [ "${buildos}" = "" ] )
 then
-    BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+	BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 else 
-    BUILDOS="${buildos}"
+	BUILDOS="${buildos}"
 fi
 
 CLOUDHOST="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
 
 if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
-    if ( [ "${BUILDOS}" = "ubuntu" ] )
-    then
-        /usr/bin/yes | /usr/bin/dpkg --configure -a
-        DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 install -y -qq apt-utils
-    fi
+	if ( [ "${BUILDOS}" = "ubuntu" ] )
+	then
+		/usr/bin/yes | /usr/bin/dpkg --configure -a
+		DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 install -y -qq apt-utils
+	fi
 
-    if ( [ "${BUILDOS}" = "debian" ] )
-    then
-        /usr/bin/yes | /usr/bin/dpkg --configure -a
-        DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 install -y -qq apt-utils
-    fi
+	if ( [ "${BUILDOS}" = "debian" ] )
+	then
+		/usr/bin/yes | /usr/bin/dpkg --configure -a
+		DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 install -y -qq apt-utils
+	fi
 fi
 
 if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
 then
-    if ( [ "${BUILDOS}" = "ubuntu" ] )
-    then
-        apt_fast_url='https://raw.githubusercontent.com/ilikenwf/apt-fast/master'
+	if ( [ "${BUILDOS}" = "ubuntu" ] )
+	then
+		apt_fast_url='https://raw.githubusercontent.com/ilikenwf/apt-fast/master'
 
-        if ( [ -f /usr/local/sbin/apt-fast ] )
-        then
-            /bin/rm -f /usr/local/sbin/apt-fast
-        fi
+		if ( [ -f /usr/local/sbin/apt-fast ] )
+		then
+			/bin/rm -f /usr/local/sbin/apt-fast
+		fi
 
-        /usr/bin/wget "${apt_fast_url}"/apt-fast -O /usr/sbin/apt-fast
-        /bin/chmod +x /usr/sbin/apt-fast
+		/usr/bin/wget "${apt_fast_url}"/apt-fast -O /usr/sbin/apt-fast
+		/bin/chmod +x /usr/sbin/apt-fast
 
-        if ( [ ! -f /etc/apt-fast.conf ] )
-        then
-            /usr/bin/wget "$apt_fast_url"/apt-fast.conf -O /etc/apt-fast.conf
-        fi
+		if ( [ ! -f /etc/apt-fast.conf ] )
+		then
+			/usr/bin/wget "$apt_fast_url"/apt-fast.conf -O /etc/apt-fast.conf
+		fi
                 
-        if ( [ -f /etc/apt/sources.list.d/ubuntu.sources ] )
-        then
-            /bin/sed -i "s/digitalocean/linode/g" /etc/apt/sources.list.d/ubuntu.sources
-        fi
+		if ( [ -f /etc/apt/sources.list.d/ubuntu.sources ] )
+		then
+			/bin/sed -i "s/digitalocean/linode/g" /etc/apt/sources.list.d/ubuntu.sources
+		fi
 
-        ${HOME}/installscripts/InstallAria2.sh "ubuntu"
-        /bin/echo 'DOWNLOADBELOW="aria2c -c -s ${_MAXNUM} -x ${_MAXNUM} -k 1M -q --file-allocation=none"' >> /etc/apt-fast.conf
-        /bin/sed -i 's/^#DOWNLOADBEFORE/DOWNLOADBEFORE/g' /etc/apt-fast.conf
-        if ( [ "${CLOUDHOST}" = "digitalocean" ] )
-        then
-            /bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
-        fi
-        if ( [ "${CLOUDHOST}" = "linode" ] )
-        then
-            /bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
-        fi
-        /bin/echo "DLLIST=" >> /etc/apt-fast.conf
-    fi
+		${HOME}/installscripts/InstallAria2.sh "ubuntu"
+		/bin/echo 'DOWNLOADBELOW="aria2c -c -s ${_MAXNUM} -x ${_MAXNUM} -k 1M -q --file-allocation=none"' >> /etc/apt-fast.conf
+		/bin/sed -i 's/^#DOWNLOADBEFORE/DOWNLOADBEFORE/g' /etc/apt-fast.conf
+		if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+		then
+			/bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
+		fi
+		if ( [ "${CLOUDHOST}" = "linode" ] )
+		then
+			/bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
+		fi
+		/bin/echo "DLLIST=" >> /etc/apt-fast.conf
+	fi
     
-    if ( [ "${BUILDOS}" = "debian" ] )
-    then
-        apt_fast_url='https://raw.githubusercontent.com/ilikenwf/apt-fast/master'
+	if ( [ "${BUILDOS}" = "debian" ] )
+	then
+		apt_fast_url='https://raw.githubusercontent.com/ilikenwf/apt-fast/master'
 
-        if ( [ -f /usr/local/sbin/apt-fast ] )
-        then
-            /bin/rm -f /usr/local/sbin/apt-fast
-        fi
+		if ( [ -f /usr/local/sbin/apt-fast ] )
+		then
+			/bin/rm -f /usr/local/sbin/apt-fast
+		fi
 
-        /usr/bin/wget "${apt_fast_url}"/apt-fast -O /usr/sbin/apt-fast
-        /bin/chmod +x /usr/sbin/apt-fast
+		/usr/bin/wget "${apt_fast_url}"/apt-fast -O /usr/sbin/apt-fast
+		/bin/chmod +x /usr/sbin/apt-fast
 
-        if ( [ ! -f /etc/apt-fast.conf ] )
-        then
-            /usr/bin/wget "$apt_fast_url"/apt-fast.conf -O /etc/apt-fast.conf
-        fi
+		if ( [ ! -f /etc/apt-fast.conf ] )
+		then
+			/usr/bin/wget "$apt_fast_url"/apt-fast.conf -O /etc/apt-fast.conf
+		fi
                 
-        if ( [ -f /etc/apt/mirrors/debian.list ] )
-        then
-            /bin/sed -i "s/digitalocean/linode/g" /etc/apt/mirrors/debian.list
-        fi
-        ${HOME}/installscripts/InstallAria2.sh "debian"
-        /bin/echo 'DOWNLOADBELOW="aria2c -c -s ${_MAXNUM} -x ${_MAXNUM} -k 1M -q --file-allocation=none"' >> /etc/apt-fast.conf
-        /bin/sed -i 's/^#DOWNLOADBEFORE/DOWNLOADBEFORE/g' /etc/apt-fast.conf
-        if ( [ "${CLOUDHOST}" = "digitalocean" ] )
-        then
-            /bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
-        fi
-        if ( [ "${CLOUDHOST}" = "linode" ] )
-        then
-            /bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
-        fi
-        /bin/echo "DLLIST=" >> /etc/apt-fast.conf
-    fi
+		if ( [ -f /etc/apt/mirrors/debian.list ] )
+		then
+			/bin/sed -i "s/digitalocean/linode/g" /etc/apt/mirrors/debian.list
+		fi
+		${HOME}/installscripts/InstallAria2.sh "debian"
+		/bin/echo 'DOWNLOADBELOW="aria2c -c -s ${_MAXNUM} -x ${_MAXNUM} -k 1M -q --file-allocation=none"' >> /etc/apt-fast.conf
+		/bin/sed -i 's/^#DOWNLOADBEFORE/DOWNLOADBEFORE/g' /etc/apt-fast.conf
+		if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+		then
+			/bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
+		fi
+		if ( [ "${CLOUDHOST}" = "linode" ] )
+		then
+			/bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
+		fi
+		/bin/echo "DLLIST=" >> /etc/apt-fast.conf
+	fi
 fi
