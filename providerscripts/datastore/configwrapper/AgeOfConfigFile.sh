a07b0071a5c4eb810f96dc7e34e145c329b6bcae
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-22 23:37:00 +0000

    Update AgeOfConfigFile.sh

--- providerscripts/datastore/configwrapper/AgeOfConfigFile.sh
+++ providerscripts/datastore/configwrapper/AgeOfConfigFile.sh
@@ -1,3 +1,25 @@
+#!/bin/sh
+####################################################################################
+# Author: Peter Winter
+# Date :  24/02/2022
+# Description: This script will tell us the "age" of a file in the config datastore
+#######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################################
+######################################################################################
+#set -x
+
 export HOME=`/bin/cat /home/homedir.dat`
 WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 
@@ -10,10 +32,10 @@ inspected_file="${config_bucket}/${1}"
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-  time_file_written="`/usr/bin/s3cmd info s3://${inspected_file}| /bin/grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
+	time_file_written="`/usr/bin/s3cmd info s3://${inspected_file}| /bin/grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
 elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
-  time_file_written="`/usr/bin/s5cmd ls s3://${inspected_file} | /usr/bin/awk '{print $1,$2}'`"
+	time_file_written="`/usr/bin/s5cmd ls s3://${inspected_file} | /usr/bin/awk '{print $1,$2}'`"
 fi
 
 time_file_written="`/usr/bin/date -d "${time_file_written}" +%s`"
