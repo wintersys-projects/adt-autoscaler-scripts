e28955c14ffbd8d4ff6bd9f1828e0c2241125709
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-23 00:17:21 +0000

    Update NumberOfServers.sh

--- providerscripts/server/NumberOfServers.sh
+++ providerscripts/server/NumberOfServers.sh
@@ -25,7 +25,7 @@ cloudhost="${2}"
 
 if ( [ -f ${HOME}/DROPLET ] || [ "${cloudhost}" = "digitalocean" ] )
 then
-        /usr/local/bin/doctl compute droplet list -o json | /usr/bin/jq -r '.[] | select (.name | contains("'${server_type}'")).id' | /usr/bin/wc -l
+	/usr/local/bin/doctl compute droplet list -o json | /usr/bin/jq -r '.[] | select (.name | contains("'${server_type}'")).id' | /usr/bin/wc -l
 fi
 
 if ( [ -f ${HOME}/EXOSCALE ] || [ "${cloudhost}" = "exoscale" ] )
