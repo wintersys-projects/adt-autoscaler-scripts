5b556c9b38bfa6faed6a68cc4b889ec9cb6bd1bd
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-25 16:35:43 +0000

    Update DestroyServer.sh

--- providerscripts/server/DestroyServer.sh
+++ providerscripts/server/DestroyServer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-############################################################################################################################
+#######################################################################################################
 # Author : Peter Winter
 # Date   : 13/06/2016
-# Description : This script destroys a machine based on ip address
-########################################################################################################################
+# Description : This script destroys a machine based on ip address and cleans up after it is destroyed
+#######################################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
