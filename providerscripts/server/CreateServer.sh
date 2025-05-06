318b983d119325143fa3cd182329658af10166f5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-06 08:19:32 +0100

    Update CreateServer.sh

--- providerscripts/server/CreateServer.sh
+++ providerscripts/server/CreateServer.sh
@@ -114,7 +114,7 @@ then
 	fi
 
 	#vpc_id="`/usr/bin/vultr vpc2 list -o json | /usr/bin/jq -r '.vpcs[] | select (.description == "adt-vpc").id'`"
-	vpc_id="`/usr/bin/vultr vpc list -o json | /usr/bin/jq -r '.vpcs[] | select (.description == "${VPC_NAME}").id'`"
+	vpc_id="`/usr/bin/vultr vpc list -o json | /usr/bin/jq -r '.vpcs[] | select (.description == "'${VPC_NAME}'").id'`"
 
 	firewall_id="`/usr/bin/vultr firewall group list -o json | /usr/bin/jq -r '.firewall_groups[] | select (.description == "adt-autoscaler-'${BUILD_IDENTIFIER}'").id'`"
 
