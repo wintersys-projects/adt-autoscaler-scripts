7c8567dfd90b267136f9f3bb9c2728f26db41dec
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-06 20:09:41 +0100

    Update DoubleCheckConfig.sh

--- autoscaler/DoubleCheckConfig.sh
+++ autoscaler/DoubleCheckConfig.sh
@@ -31,25 +31,27 @@ then
 	then
 		config_status="ok"
 	fi	
-	if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:wordpress`" = "1" ] )
-	then
-		if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/wp-config.php | /bin/grep -E 'HTTP*404'`" != "" ] )
-		then
-			config_status="ok"
-		fi	
-	if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:drupal`" = "1" ] )
-	then 
-		if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/sites/default/settings.php | /bin/grep -E 'HTTP*404'`" != "" ] )
-		then
-			config_status="ok"
-		fi	
-	fi
-	if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:moodle`" = "1" ] )
-	then
-		if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/moodle/config.php | /bin/grep -E 'HTTP*404'`" != "" ] )
-		then
-			config_status="ok"
-		fi	
-	fi
 fi
+if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:wordpress`" = "1" ] )
+then
+	if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/wp-config.php | /bin/grep -E 'HTTP*404'`" != "" ] )
+	then
+		config_status="ok"
+	fi	
+fi
+if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:drupal`" = "1" ] )
+then 
+	if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/sites/default/settings.php | /bin/grep -E 'HTTP*404'`" != "" ] )
+	then
+		config_status="ok"
+	fi	
+fi
+if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:moodle`" = "1" ] )
+then
+	if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/moodle/config.php | /bin/grep -E 'HTTP*404'`" != "" ] )
+	then
+		config_status="ok"
+	fi	
+fi
+
 /bin/echo "${config_status}"
