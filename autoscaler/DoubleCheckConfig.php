9f89314b3efb103bf4194e6a240702c87ae08933
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-06 19:36:52 +0100

    Update DoubleCheckConfig.php

--- autoscaler/DoubleCheckConfig.php
+++ autoscaler/DoubleCheckConfig.php
@@ -1,3 +1,6 @@
 
 
-if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/${headfile} | /bin/grep -E 'HTTP/2 200|HTTP/2 301|HTTP/2 302|HTTP/2 303|200 OK|302 Found|301 Moved Permanently'`" = "" ] )
+if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/configuration.php | /bin/grep -E 'HTTP*404'`" != "" ] )
+then
+
+fi
