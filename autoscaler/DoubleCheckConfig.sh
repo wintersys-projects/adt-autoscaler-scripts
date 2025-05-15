345f50a97d357773d3de1ab0d3d636ac291e5e92
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-15 18:19:08 +0100

    Update DoubleCheckConfig.sh

--- autoscaler/DoubleCheckConfig.sh
+++ autoscaler/DoubleCheckConfig.sh
@@ -27,28 +27,28 @@ config_status="not ok"
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:joomla`" = "1" ] )
 then
-	if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/configuration.php | /bin/grep -E 'HTTP*404'`" = "" ] )
+	if ( [ "`/usr/bin/curl -s -I --max-time 60 --insecure https://${private_ip}:443/configuration.php | /bin/grep -E 'HTTP*404'`" = "" ] )
 	then
 		config_status="ok"
 	fi	
 fi
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:wordpress`" = "1" ] )
 then
-	if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/wp-config.php | /bin/grep -E 'HTTP*404'`" = "" ] )
+	if ( [ "`/usr/bin/curl -s -I --max-time 60 --insecure https://${private_ip}:443/wp-config.php | /bin/grep -E 'HTTP*404'`" = "" ] )
 	then
 		config_status="ok"
 	fi	
 fi
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:drupal`" = "1" ] )
 then 
-	if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/sites/default/settings.php | /bin/grep -E 'HTTP*404'`" = "" ] )
+	if ( [ "`/usr/bin/curl -s -I --max-time 60 --insecure https://${private_ip}:443/sites/default/settings.php | /bin/grep -E 'HTTP*404'`" = "" ] )
 	then
 		config_status="ok"
 	fi	
 fi
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:moodle`" = "1" ] )
 then
-	if ( [ "`/usr/bin/curl -I --max-time 60 --insecure https://${private_ip}:443/config.php | /bin/grep -E 'HTTP*404'`" = "" ] )
+	if ( [ "`/usr/bin/curl -s -I --max-time 60 --insecure https://${private_ip}:443/config.php | /bin/grep -E 'HTTP*404'`" = "" ] )
 	then
 		config_status="ok"
 	fi	
