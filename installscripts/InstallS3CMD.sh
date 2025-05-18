705e23f142178f3ae8970303566b7bf5846ae7ab
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-18 17:55:29 +0100

    Update InstallS3CMD.sh

--- installscripts/InstallS3CMD.sh
+++ installscripts/InstallS3CMD.sh
@@ -51,11 +51,13 @@ then
 		if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd:repo'`" = "1" ] )
  		then
 			eval ${install_command} s3cmd	
-		elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd:source'`" = "1" ] )
+		elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd:binary'`" = "1" ] )
  		then
-			/usr/bin/get clone  https://github.com/s3tools/s3cmd.git
-			/bin/cp ./s3tools/s3cmd /usr/bin/s3cmd
-			/bin/cp -r ./s3tools/S3 /usr/bin/
+  			eval ${install_command} python3
+			/usr/bin/ln -s /usr/bin/python3 /usr/bin/python
+   			/usr/bin/git clone https://github.com/s3tools/s3cmd.git
+			/bin/cp ./s3cmd/s3cmd /usr/bin/s3cmd
+			/bin/cp -r ./s3cmd/S3 /usr/bin/
 			/bin/rm -r ./s3tools
 		fi
   	fi
@@ -64,11 +66,13 @@ then
 		if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd:repo'`" = "1" ] )
 		then
 			eval ${install_command} s3cmd
-		elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd:source'`" = "1" ] )
+		elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd:binary'`" = "1" ] )
 		then
-   			/usr/bin/get clone https://github.com/s3tools/s3cmd.git
-			/bin/cp ./s3tools/s3cmd /usr/bin/s3cmd
-			/bin/cp -r ./s3tools/S3 /usr/bin/
+  			eval ${install_command} python3
+			/usr/bin/ln -s /usr/bin/python3 /usr/bin/python
+   			/usr/bin/git clone https://github.com/s3tools/s3cmd.git
+			/bin/cp ./s3cmd/s3cmd /usr/bin/s3cmd
+			/bin/cp -r ./s3cmd/S3 /usr/bin/
 			/bin/rm -r ./s3tools
 		fi
 	fi
