13e7d481ba7b5ec2e57227dece47439eed87df16
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-18 21:33:47 +0100

    Update InstallSSMTP.sh

--- installscripts/InstallSSMTP.sh
+++ installscripts/InstallSSMTP.sh
@@ -55,6 +55,12 @@ then
 	then
 		eval ${install_command} ssmtp	
 		eval ${install_command} mailutils	
-	fi
+	fi	
+fi
+
+if ( [ ! -f /usr/sbin/ssmtp] )
+then
+	${HOME}/providerscripts/email/SendEmail.sh "INSTALLATION ERROR SSMTP" "I believe that ssmtp hasn't installed correctly, please investigate" "ERROR"
+else
 	/bin/touch ${HOME}/runtime/installedsoftware/InstallSSMPT.sh	
 fi
