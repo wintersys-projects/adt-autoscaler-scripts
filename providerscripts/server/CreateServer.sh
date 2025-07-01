c2f79ce23bae5bb23b986b9c5829257b7b9026de
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-01 23:52:38 +0100

    Update CreateServer.sh

--- providerscripts/server/CreateServer.sh
+++ providerscripts/server/CreateServer.sh
@@ -53,11 +53,11 @@ then
 	/bin/sed -i "s/XXXXWEBSERVER_HOSTNAMEXXXX/${server_name}/g" ${HOME}/runtime/cloud-init/webserver.yaml
 	cloud_config="`/bin/cat ${HOME}/runtime/cloud-init/webserver.yaml`"
 
-	image='--image "${OS_CHOICE}"'
+	image='--image "'${OS_CHOICE}'"'
 
  	if ( [ "${SNAPSHOT_ID}" != "" ] )
   	then
-   		image='--image "${SNAPSHOT_ID}"'
+   		image='--image "'${SNAPSHOT_ID}'"'
         fi
         
 	webserver_id="`/usr/local/bin/doctl compute droplet create "${server_name}" -o json --size "${server_size}" ${image} --region "${REGION}" --ssh-keys "${KEY_ID}" --vpc-uuid "${vpc_id}" --user-data "${cloud_config}" | /usr/bin/jq -r '.[].id'`"
