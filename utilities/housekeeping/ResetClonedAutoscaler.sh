c8bb5c51016d566147bce789945cddf1184da09a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-01 09:29:26 +0100

    Update ResetClonedAutoscaler.sh

--- utilities/housekeeping/ResetClonedAutoscaler.sh
+++ utilities/housekeeping/ResetClonedAutoscaler.sh
@@ -1,14 +1,13 @@
 HOME="`/bin/cat /home/homedir.dat`"
 BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+CLOUDHOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
+
 
 /bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
 
-#USER_NAME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
-
-#original_user="`/bin/ls -l /home | /bin/grep "X*X" | /usr/bin/awk '{print $NF}' | /bin/grep -v "${USER_NAME}"`"
-
-#/usr/bin/rsync -avrP /home/${original_user}/* ${HOME}/ --exclude=".ssh" --include='.*' --ignore-existing 
-
-#/bin/rm -r /home/${original_user}
+if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+then
+  ${HOME}/utilities/processing/RunServiceCommand.sh "snapd.apparmor" restart
+fi
 
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &
