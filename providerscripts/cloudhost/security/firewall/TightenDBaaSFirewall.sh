779c862bbb9868f1de18c90b3594d7798ba2c2a1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-05 20:46:13 +0100

    Update TightenDBaaSFirewall.sh

--- providerscripts/cloudhost/security/firewall/TightenDBaaSFirewall.sh
+++ providerscripts/cloudhost/security/firewall/TightenDBaaSFirewall.sh
@@ -144,10 +144,20 @@ then
           selected_databaseid="${databaseid}"
       fi
   done
+    ipaddresses=""
+    if ( [ "${multi_region_ips}" != "" ] )
+    then
+       ipaddresses="["
+       for ip in ${multi_region_ips}
+       do
+            ipaddresses="${ipaddresses}${ip},"
+       done
+       ipaddresses="`/bin/echo ${ipaddresses} | /bin/sed 's/,$//'`]"
+    fi
 
   if ( [ "${selected_databaseid}" != "" ] )
   then
       VPC_IP_RANGE="`${HOME}/utilities/config/ExtractConfigValue.sh 'VPCIPRANGE'`"
-      /usr/bin/vultr database update ${selected_databaseid} --trusted-ips="${VPC_IP_RANGE}"
+      /usr/bin/vultr database update ${selected_databaseid} --trusted-ips="${ipaddresses}"
   fi
 fi
