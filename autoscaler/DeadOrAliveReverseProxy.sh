e016322b2b5952af818223cb2ab9f3a9f2ccfb38
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-11 20:36:44 +0100

    Update DeadOrAliveReverseProxy.sh

--- autoscaler/DeadOrAliveReverseProxy.sh
+++ autoscaler/DeadOrAliveReverseProxy.sh
@@ -1,5 +1,28 @@
 
 
+probe_by_curl()
+{
+        probecount="0"
+        status="down"
+        file="`${HOME}/autoscaler/SelectHeadFile.sh`"
+        while ( [ "${probecount}" -le "3" ] && [ "${status}" = "down" ] )
+        do
+                if ( [ "`/usr/bin/curl -s -m 20 --insecure -I "https://${ip}:443/${file}" 2>&1 | /bin/grep "HTTP" | /bin/grep -E "200|301|302|303"`" != "" ] ) 
+                then
+                        status="up"
+                else
+                        status="down"
+                fi
+                probecount="`/usr/bin/expr ${probecount} + 1`"
+        done
+
+        if ( [ "${status}" = "down" ] )
+        then
+                /bin/echo "${0} `/bin/date`: Webserver ${ip} was found to be offline because it couldn't be contacted using curl" 
+                /bin/echo "${ip}" >> ${HOME}/runtime/probed_ips/failed_probe_curl_ips.dat
+        fi
+}
+
 CLOUDHOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
 BUILD_IDENTIFIER="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
 REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'REGION'`"
@@ -8,5 +31,5 @@ ips="`${HOME}/providerscripts/server/GetServerIPAddresses.sh "rp-${REGION}-${BUI
 
 for ip in ${ips}
 do
-
+  probe_by_curl
 done
