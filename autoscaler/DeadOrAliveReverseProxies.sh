df666e2f5c21ae4f529b39c2f6cc4463f274ef41
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-14 08:24:52 +0100

    Update DeadOrAliveReverseProxies.sh

--- autoscaler/DeadOrAliveReverseProxies.sh
+++ autoscaler/DeadOrAliveReverseProxies.sh
@@ -27,7 +27,7 @@ CLOUDHOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
 BUILD_IDENTIFIER="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
 REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'REGION'`"
 
-ips="`${HOME}/providerscripts/server/GetServerIPAddresses.sh "rp-${REGION}-${BUILD_IDENTIFIER}" ${CLOUDHOST}`"
+ips="`${HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "rp-${REGION}-${BUILD_IDENTIFIER}" ${CLOUDHOST}`"
 
 for ip in ${ips}
 do
