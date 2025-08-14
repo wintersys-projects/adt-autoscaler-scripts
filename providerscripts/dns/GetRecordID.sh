43181e189975049b4923ac444a4e7cd071316dcf
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-08-14 08:59:07 +0100

    Update GetRecordID.sh

--- providerscripts/dns/GetRecordID.sh
+++ providerscripts/dns/GetRecordID.sh
@@ -30,6 +30,7 @@ dns="${6}"
 
 if ( [ "${dns}" = "cloudflare" ] )
 then
+	api_token="`/bin/echo ${credentials} | /usr/bin/awk -F':::' '{print $2}'`"
 	#/usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records?type=A&name=${websiteurl}&content=${ip}&page=1&per_page=20&order=type&direction=desc&match=all" -H "X-Auth-Email: ${email}" -H "X-Auth-Key: ${authkey}" -H "Content-Type: application/json" | /usr/bin/jq -r '.result[].id'
 	/usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records?type=A&name=${websiteurl}&content=${ip}&page=1&per_page=20&order=type&direction=desc&match=all" -H "X-Auth-Email: ${email}" -H "X-Auth-Key: ${authkey}" -H "Content-Type: application/json" | /usr/bin/jq -r '.result[].id'
 fi
