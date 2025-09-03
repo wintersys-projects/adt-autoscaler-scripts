394a3641b97042a06499444789080cfdef0ee20e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-03 11:09:11 +0100

    Update InstallPackageManager.sh

--- installscripts/InstallPackageManager.sh
+++ installscripts/InstallPackageManager.sh
@@ -73,6 +73,7 @@ then
 			/bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
 		fi
 		/bin/echo "DLLIST=" >> /etc/apt-fast.conf
+		/bin/touch ${HOME}/runtime/installedsoftware/InstallPackageManager.sh
 	fi
 
 	if ( [ "${BUILDOS}" = "debian" ] )
@@ -96,5 +97,6 @@ then
 			/bin/echo "MIRRORS=( 'mirrors.linode.com' )" >> /etc/apt-fast.conf
 		fi
 		/bin/echo "DLLIST=" >> /etc/apt-fast.conf
+		/bin/touch ${HOME}/runtime/installedsoftware/InstallPackageManager.sh
 	fi
 fi
