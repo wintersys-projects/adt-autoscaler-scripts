2852532e0440cffc4d8a3801bb7ce20f8881fa33
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-21 21:27:57 +0100

    Update AddRecord.sh

--- providerscripts/dns/AddRecord.sh
+++ providerscripts/dns/AddRecord.sh
@@ -77,9 +77,8 @@ dns="${6}"
 
 if ( [ "${dns}" = "exoscale" ] )
 then
-	zone="`${HOME}/utilities/config/ExtractConfigValue.sh 'REGION'`"
- 	count="0"
-	while ( [ "$?" != "0" ] && ( [ "${count}" -lt "5" ] || [ "${count}" = "0" ] ) )
+	count="0"
+	while ( [ "${count}" -lt "5" ] && [ "`/usr/bin/exo dns list -O json | /usr/bin/jq -r '.[] | select (.content ="'${ip}'").id'`" = "" ] )
  	do
   		count="`/usr/bin/expr ${count} + 1`"
 		/usr/bin/exo dns add A ${domainurl} -a ${ip} -n ${subdomain} -t 60
