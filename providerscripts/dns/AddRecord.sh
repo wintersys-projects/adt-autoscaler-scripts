898815e907042f6b9988e76de29a000b7eb3fb17
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-05 11:53:03 +0000

    Update AddRecord.sh

--- providerscripts/dns/AddRecord.sh
+++ providerscripts/dns/AddRecord.sh
@@ -29,23 +29,14 @@ dns="${6}"
 
 if ( [ "${dns}" = "cloudflare" ] )
 then
-	#authkey="${credentials}"
-	api_token="`/bin/echo ${credentials} | /usr/bin/awk -F':::' '{print $2}'`"
-	#Make damn sure that the DNS record gets added to the DNS system
- 	count="0"
-	while ( ( [ "$?" != "0" ] || [ "${count}" = "0" ] ) && [ "${count}" -lt "5" ] )
- 	do
-  		count="`/usr/bin/expr ${count} + 1`"
-		#key
-		#/usr/bin/curl -X POST "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records" -H "X-Auth-Email: ${email}" -H "X-Auth-Key: ${authkey}" -H "Content-Type: application/json" --data '{"type":"A","name":"'${websiteurl}'","content":"'${ip}'","proxiable":true,"proxied":'${proxied}',"ttl":120}'
-		#token
-  		/usr/bin/curl -X POST "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records" --header "Authorization: Bearer ${api_token}" --header "Content-Type: application/json" --data '{"type":"A","name":"'${websiteurl}'","content":"'${ip}'","proxiable":true,"proxied":true,"ttl":120}'
-	done
- 
- 	if ( [ "${count}" = "5" ] )
-  	then
-		${HOME}/providerscripts/email/SendEmail.sh "FAILED TO ADD IP ADDRESS TO DNS SYSTEM" "IP address (${ip}) for domain ${domainurl}) could not be added to the DNS system" "ERROR"
-	fi
+        if ( [ "`/bin/echo ${credentials} | /bin/grep ':::'`" != "" ] )
+        then
+                api_token="`/bin/echo ${credentials} | /usr/bin/awk -F':::' '{print $2}'`"
+                /usr/bin/curl -X POST "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records" --header "Authorization: Bearer ${api_token}" --header "Content-Type: application/json" --data '{"type":"A","name":"'${websiteurl}'","content":"'${ip}'","proxiable":true,"proxied":'${proxied}',"ttl":120}'
+        else
+                authkey="${credentials}"
+                /usr/bin/curl -X POST "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records" -H "X-Auth-Email: ${email}" -H "X-Auth-Key: ${authkey}" -H "Content-Type: application/json" --data '{"type":"A","name":"'${websiteurl}'","content":"'${ip}'","proxiable":true,"proxied":'${proxied}',"ttl":120}'
+        fi
 fi
 
 websiteurl="${4}"
