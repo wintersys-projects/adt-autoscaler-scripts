36cfd101b17268fa4ad3c65c4c7e53a5b870777d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-05 13:43:34 +0000

    Update GetRecordID.sh

--- providerscripts/dns/GetRecordID.sh
+++ providerscripts/dns/GetRecordID.sh
@@ -30,11 +30,15 @@ dns="${6}"
 
 if ( [ "${dns}" = "cloudflare" ] )
 then
-	api_token="`/bin/echo ${credentials} | /usr/bin/awk -F':::' '{print $2}'`"
-	#/usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records?type=A&name=${websiteurl}&content=${ip}&page=1&per_page=20&order=type&direction=desc&match=all" -H "X-Auth-Email: ${email}" -H "X-Auth-Key: ${authkey}" -H "Content-Type: application/json" | /usr/bin/jq -r '.result[].id'
-	/usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records?type=A&name=${websiteurl}&content=${ip}&page=1&per_page=20&order=type&direction=desc&match=all" --header "Authorization: Bearer ${api_token}" --header "Content-Type: application/json" | /usr/bin/jq -r '.result[].id'
+        if ( [ "`/bin/echo ${credentials} | /usr/bin/awk -F':::' '{print $2}'`" != "" ] )
+        then
+                api_token="`/bin/echo ${credentials} | /usr/bin/awk -F':::' '{print $2}'`"
+                /usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records?type=A&name=${websiteurl}&content=${ip}&page=1&per_page=20&order=type&direction=desc&match=all" --header "Authorization: Bearer ${api_token}" --header "Content-Type: application/json" | /usr/bin/jq -r '.result[].id'
+        else
+                authkey="${credentials}"
+                /usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records?type=A&name=${websiteurl}&content=${ip}&page=1&per_page=20&order=type&direction=desc&match=all" -H "X-Auth-Email: ${email}" -H "X-Auth-Key: ${authkey}" -H "Content-Type: application/json" | /usr/bin/jq -r '.result[].id'
+        fi
 fi
-
 
 websiteurl="${2}"
 domainurl="`/bin/echo ${2} | /usr/bin/cut -d'.' -f2-`"
