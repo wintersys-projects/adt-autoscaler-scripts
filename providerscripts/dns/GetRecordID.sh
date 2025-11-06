c2c3c4b40c1111332d68e82b810384c0adf14989
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-06 13:02:03 +0000

    Update GetRecordID.sh

--- providerscripts/dns/GetRecordID.sh
+++ providerscripts/dns/GetRecordID.sh
@@ -59,7 +59,7 @@ dns="${6}"
 
 if ( [ "${dns}" = "exoscale" ] )
 then
-        /usr/bin/exo dns show ${domainurl}  --config /root/.config/exoscale/.dns-exoscale.toml -O json | /usr/bin/jq -r '.[] | select (.content == "'${ip}'").id'
+        /usr/bin/exo dns show ${domainurl}  --config /root/.config/exoscale/dns-exoscale.toml -O json | /usr/bin/jq -r '.[] | select (.content == "'${ip}'").id'
         #Alternative
         #/usr/bin/curl  -H "X-DNS-Token: ${authkey}" -H 'Accept: application/json' https://api.exoscale.com/dns/v1/domains/${domainurl}/records | /usr/bin/jq --arg tmp_subdomain "${subdomain}" --arg tmp_content "${ip}" '.[].record | select (.name == $tmp_subdomain and .content == $tmp_content ) | .id'
 fi
