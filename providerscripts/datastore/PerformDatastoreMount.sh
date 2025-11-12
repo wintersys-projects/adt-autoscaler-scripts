d9edb3964011a073655dc7ac074c668ca4fba5ac
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-12 16:27:59 +0000

    Create PerformDatastoreMount.sh

---
+++ providerscripts/datastore/PerformDatastoreMount.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#########################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: Create a bucket in the datastore
+##########################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#########################################################################################
+#########################################################################################
+#set -x
+
+datastore_to_mount="$1"
+count="${2}"
+
+if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
+then
+        datastore_tool="/usr/bin/s3cmd --config=/root/.s3cfg-${count} "
+elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
+then
+        host_base="`/bin/grep host_base /root/.s5cfg-${count} | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg-${count} --endpoint-url https://${host_base} "
+fi
+
+if ( [ "`${datastore_tool} ls s3://${datastore_to_mount} 2>&1 >/dev/null | /bin/grep "ERROR"`" = "" ] )
+then
+        exit
+fi
+
+count="0"
+while ( [ "`${datastore_tool} mb s3://${datastore_to_mount} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
+do
+        /bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
+        /bin/sleep 5
+        count="`/usr/bin/expr ${count} + 1`"
+done 
