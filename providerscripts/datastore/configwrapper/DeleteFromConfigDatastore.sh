119f151d6715e9605f0ead2072c8076faf0d4b37
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-17 19:23:31 +0000

    Update DeleteFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh
@@ -43,7 +43,7 @@ then
 elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
 	host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-	datastore_cmd="${datastore_tool}--credentials-file /root/.s5cfg --endpoint-url https://${host_base} rm "
+	datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg --endpoint-url https://${host_base} rm "
 fi
 
 count="0"
