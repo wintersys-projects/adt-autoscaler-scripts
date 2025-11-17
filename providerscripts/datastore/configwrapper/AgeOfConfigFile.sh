cb57d3ce12b0be92aa641b88fc6476519299ce41
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-17 17:31:17 +0000

    Update AgeOfConfigFile.sh

--- providerscripts/datastore/configwrapper/AgeOfConfigFile.sh
+++ providerscripts/datastore/configwrapper/AgeOfConfigFile.sh
@@ -32,11 +32,23 @@ inspected_file="${config_bucket}/${1}"
 
 if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-	time_file_written="`/usr/bin/s3cmd info s3://${inspected_file}| /bin/grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
+	datastore_tool="/usr/bin/s3cmd"
 elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
-	host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-	time_file_written="`/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} ls s3://${inspected_file} | /usr/bin/awk '{print $1,$2}'`"
+	datastore_tool="/usr/bin/s5cmd"
+fi
+
+if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
+then
+	config_file="`/bin/grep -H ${datastore_region} /root/.s3cfg-* | /usr/bin/awk -F':' '{print $1}'`"
+	datastore_cmd="${datastore_tool} --config=${config_file} info s3://${inspected_file}"
+	time_file_written="`${datastore_cmd} | /bin/grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
+elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
+then
+	config_file="`/bin/grep -H ${datastore_region} /root/.s5cfg-* | /usr/bin/awk -F':' '{print $1}'`"
+	host_base="`/bin/grep host_base ${config_file}| /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+	datastore_cmd="${datastore_tool} --credentials-file  ${config_file} --endpoint-url https://${host_base} head s3://${inspected_file}"
+	time_file_written="`${datastore_cmd} | /usr/bin/jq -r '.last_modified'`"
 fi
 
 time_file_written="`/usr/bin/date -d "${time_file_written}" +%s`"
