0b7824b07a9db8b357d56bc1f283bdadd3002a04
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-24 15:42:17 +0000

    Update DeadOrAliveWebservers.sh

--- autoscaler/DeadOrAliveWebservers.sh
+++ autoscaler/DeadOrAliveWebservers.sh
@@ -82,7 +82,7 @@ endit ()
 		autoscalerip="`${HOME}/utilities/processing/GetPublicIP.sh`"
 		public_ip_address="`${HOME}/providerscripts/server/GetServerPublicIPAddressByIP.sh ${down_ip} ${CLOUDHOST}`"
 
-		if ( [ "`${HOME}/providerscripts/server/GetServerName.sh "${public_ip_address}" "${CLOUDHOST}" | /bin/grep "init"`" = "" ] || [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh "beingbuiltips/*" 2>/dev/null | /bin/grep ${down_ip}`" = "" ] || [ "`/usr/bin/find ${HOME}/runtime/POTENTIAL_STALLED_BUILD:${ip} -mmin +30`" != "" ] )
+		if ( [ "`${HOME}/providerscripts/server/GetServerName.sh "${public_ip_address}" "${CLOUDHOST}" | /bin/grep "\-init\-"`" = "" ] || [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh "beingbuiltips/*" 2>/dev/null | /bin/grep ${down_ip}`" = "" ] || [ "`/usr/bin/find ${HOME}/runtime/POTENTIAL_STALLED_BUILD:${ip} -mmin +30`" != "" ] )
 		then
 			/bin/echo "Ending server with ip address ${down_ip}"
 
