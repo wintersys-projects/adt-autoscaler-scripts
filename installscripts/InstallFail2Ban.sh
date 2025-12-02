25baac662996795119a7f9eef35347a9a3981087
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-02 22:58:13 +0000

    Update InstallFail2Ban.sh

--- installscripts/InstallFail2Ban.sh
+++ installscripts/InstallFail2Ban.sh
@@ -54,7 +54,7 @@ then
 		if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "FAIL2BAN" | /usr/bin/awk -F':' '{print $NF}'`" = "active" ] )
 		then
     		eval ${install_command} fail2ban
-			/bin/cp ${HOME}/security/config/fail2ban.conf /etc/fail2ban/jail.local
+			/bin/cp ${HOME}/security/config/fail2ban.conf /etc/fail2ban/jail.d/jail.local
 		fi
 	fi
 
@@ -63,7 +63,7 @@ then
 		if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "FAIL2BAN" | /usr/bin/awk -F':' '{print $NF}'`" = "active" ] )
 		then
     		eval ${install_command} fail2ban
-			/bin/cp ${HOME}/security/config/fail2ban.conf /etc/fail2ban/jail.local
+			/bin/cp ${HOME}/security/config/fail2ban.conf /etc/fail2ban/jail.d/jail.local
 		fi
 	fi
 fi
