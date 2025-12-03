f0e4bcf54af58432d833ecb585eb640b939160d8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-03 18:26:49 +0000

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -37,7 +37,7 @@ then
 	fi
 fi
 
-if ( [ -f /etc/fail2ban/jail.d ] )
+if ( [ -d /etc/fail2ban/jail.d ] )
 then
 	/bin/cp ${HOME}/security/config/fail2ban.conf /etc/fail2ban/jail.d/jail.local
 	/bin/sed -i "s/XXXXSSHPORTXXXX/${SSH_PORT}/g" /etc/fail2ban/jail.d/jail.local
