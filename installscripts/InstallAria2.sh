95ae16f78e9917d8e9fec1298d05ce8b60c4747e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-25 22:44:54 +0000

    Update InstallAria2.sh

--- installscripts/InstallAria2.sh
+++ installscripts/InstallAria2.sh
@@ -59,7 +59,7 @@ do
 	count="`/usr/bin/expr ${count} + 1`"
 done
 
-if ( [ ! -f /usr/sbin/aria2c ] && [ "${count}" = "5" ] )
+if ( [ ! -x /usr/sbin/aria2c ] && [ "${count}" = "5" ] )
 then
 	${HOME}/providerscripts/email/SendEmail.sh "INSTALLATION ERROR ARIA2C" "I believe that aria2c hasn't installed correctly, please investigate" "ERROR"
 else
