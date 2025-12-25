1cc0595e5175c12793069d38703e9f72ae1d643b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-25 22:48:25 +0000

    Update InstallVultr.sh

--- installscripts/InstallVultr.sh
+++ installscripts/InstallVultr.sh
@@ -72,7 +72,7 @@ do
 	count="`/usr/bin/expr ${count} + 1`"
 done
 
-if ( [ ! -f /usr/bin/vultr ] && [ "${count}" = "5" ] )
+if ( [ ! -x /usr/bin/vultr ] && [ "${count}" = "5" ] )
 then
 	${HOME}/providerscripts/email/SendEmail.sh "INSTALLATION ERROR VULTR" "I believe that vultr hasn't installed correctly, please investigate" "ERROR"
 else
