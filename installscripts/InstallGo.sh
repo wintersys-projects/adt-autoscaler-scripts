245b6b68263d9763cb3b94d06227bed96ab7dae2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-25 22:46:11 +0000

    Update InstallGo.sh

--- installscripts/InstallGo.sh
+++ installscripts/InstallGo.sh
@@ -84,7 +84,7 @@ do
 	count="`/usr/bin/expr ${count} + 1`"
 done
 
-if ( [ ! -f /usr/bin/go ] && [ "${count}" = "5" ] )
+if ( [ ! -x /usr/bin/go ] && [ "${count}" = "5" ] )
 then
 	${HOME}/providerscripts/email/SendEmail.sh "INSTALLATION ERROR GO" "I believe that go hasn't installed correctly, please investigate" "ERROR"
 else
