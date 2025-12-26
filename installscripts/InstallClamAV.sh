d8fdce0cb1e327c072e524d8290bef6d5770182d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-26 16:28:03 +0000

    Update InstallClamAV.sh

--- installscripts/InstallClamAV.sh
+++ installscripts/InstallClamAV.sh
@@ -18,54 +18,58 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
+#set -x
 
 if ( [ "${1}" != "" ] )
 then
-	buildos="${1}"
+        buildos="${1}"
 fi
 
 if ( [ "${buildos}" = "" ] )
 then
-	BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+        BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 else 
-	BUILDOS="${buildos}"
+        BUILDOS="${buildos}"
 fi
 HOME="`/bin/cat /home/homedir.dat`"
 
 apt=""
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
-	apt="/usr/bin/apt"
+        apt="/usr/bin/apt"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-get" ] )
 then
-	apt="/usr/bin/apt-get"
+        apt="/usr/bin/apt-get"
 fi
 
 export DEBIAN_FRONTEND=noninteractive
 install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install "
 
 count="0"
-while ( [ ! -f /usr/bin/ufw ] && [ "${count}" -lt "5" ] )
+while ( ( [ ! -x /usr/bin/freshclam ] || [ ! -x /usr/bin/clamscan ] ) && [ "${count}" = "5" ] )
 do
-	if ( [ "${apt}" != "" ] )
-	then
-		if ( [ "${BUILDOS}" = "ubuntu" ] )
-		then
-			eval ${install_command} clamav clamav-daemon clamav-freshclam clamdscan
-		fi
+        if ( [ "${apt}" != "" ] )
+        then
+                if ( [ "${BUILDOS}" = "ubuntu" ] )
+                then
+                        eval ${install_command} clamav clamav-daemon clamav-freshclam clamdscan
+                fi
 
-		if ( [ "${BUILDOS}" = "debian" ] )
-		then
-			eval ${install_command} clamav clamav-daemon clamav-freshclam clamdscan
-		fi
-	fi
-	count="`/usr/bin/expr ${count} + 1`"
+                if ( [ "${BUILDOS}" = "debian" ] )
+                then
+                        eval ${install_command} clamav clamav-daemon clamav-freshclam clamdscan
+                fi
+        fi
+        count="`/usr/bin/expr ${count} + 1`"
 done
 
 
-if ( [ ! -x /usr/bin/clamav] && [ "${count}" = "5" ] )
+if ( ( [ ! -x /usr/bin/freshclam ] || [ ! -x /usr/bin/clamscan ] ) && [ "${count}" = "5" ] )
 then
-	${HOME}/providerscripts/email/SendEmail.sh "INSTALLATION ERROR ClamAV" "I believe that ClamAV hasn't installed correctly, please investigate" "ERROR"
+        ${HOME}/providerscripts/email/SendEmail.sh "INSTALLATION ERROR ClamAV" "I believe that ClamAV hasn't installed correctly, please investigate" "ERROR"
 else
-	/bin/touch ${HOME}/runtime/installedsoftware/InstallClamAV.sh	
+        /bin/touch ${HOME}/runtime/installedsoftware/InstallClamAV.sh
 fi
+
+${HOME}/utilities/processing/RunServiceCommand.sh clamav-freshclam stop
+/usr/bin/freshclam
