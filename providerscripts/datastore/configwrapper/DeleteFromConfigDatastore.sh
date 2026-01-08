093b8a08a69c8deabdf4fce5cfe76e19c589270c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-08 11:48:04 +0000

    Update DeleteFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh
@@ -23,6 +23,18 @@
 file_to_delete="$1"
 recursive="$2"
 
+if ( [ "${recursive}" = "yes" ] )
+then
+        /bin/rm -r /var/lib/adt-config/${file_to_delete} 2>/dev/null
+else
+        /bin/rm /var/lib/adt-config/${file_to_delete} 2>/dev/null
+fi
+
+if ( [ "$?" != "0" ] )
+then
+        exit
+fi
+
 export HOME=`/bin/cat /home/homedir.dat`
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
