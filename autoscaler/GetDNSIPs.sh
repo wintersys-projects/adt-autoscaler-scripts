39c915e51bda41b9dc5db24b8f9d6e4225f9d653
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-08 11:08:06 +0000

    Update GetDNSIPs.sh

--- autoscaler/GetDNSIPs.sh
+++ autoscaler/GetDNSIPs.sh
@@ -25,7 +25,7 @@
 #set -x
 
 #If the toolkit isn't fully installed yet, we don't want to do anything
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "INSTALLED_SUCCESSFULLY"`" = "0" ] )
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh INSTALLED_SUCCESSFULLY`" = "" ] )
 then
 	exit
 fi
