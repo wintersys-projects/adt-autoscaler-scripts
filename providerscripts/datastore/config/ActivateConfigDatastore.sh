fc9e8f8b0ee14f1846a6303c6f5acfb50bfa3a58
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-15 00:38:29 +0000

    Update ActivateConfigDatastore.sh

--- providerscripts/datastore/config/ActivateConfigDatastore.sh
+++ providerscripts/datastore/config/ActivateConfigDatastore.sh
@@ -1,3 +1,9 @@
+if ( [ ! -d /var/lib/adt-config ] )
+then
+        /bin/mkdir /var/lib/adt-config
+        ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "root" "/var/lib/adt-config"
+fi
+
 /usr/bin/inotifywait -q -m -r -e modify,delete,create /var/lib/adt-config | while read DIRECTORY EVENT FILE 
 do
         case $EVENT in
