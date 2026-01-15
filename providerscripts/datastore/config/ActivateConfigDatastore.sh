60db62b8f6af0189670d797a1779457f9e832adc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-15 21:30:39 +0000

    Update ActivateConfigDatastore.sh

--- providerscripts/datastore/config/ActivateConfigDatastore.sh
+++ providerscripts/datastore/config/ActivateConfigDatastore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#set -x
+set -x
 
 exec 1>/tmp/out
 exec 2>/tmp/err
@@ -14,18 +14,13 @@ then
         /bin/mkdir /var/lib/adt-config-workarea
 fi
 
-
 monitor_for_datastore_changes() {
-
-
         while ( [ 1 ] )
         do
                 /bin/sleep 5
-
                 ${HOME}/providerscripts/datastore/config/tooling/SyncFromConfigDatastoreWithDelete.sh "root" "/var/lib/adt-config"
         done
 }
-
 
 monitor_for_datastore_changes &
 
@@ -33,22 +28,31 @@ file_removed() {
         live_dir="${1}"
         deleted_file="${2}"
 
-        file_to_delete="`/bin/echo ${live_dir}${deleted_file} | /bin/sed -e 's:/var/lib/adt-config/::' -e 's://:/:'`"
-        ${HOME}/providerscripts/datastore/config/tooling/DeleteFromConfigDatastore.sh "${file_to_delete}" "no" "no"
+        if ( [ ! -f ${live_dir}${deleted_file} ] )
+        then
+                file_to_delete="`/bin/echo ${live_dir}${deleted_file} | /bin/sed -e 's:/var/lib/adt-config/::' -e 's://:/:'`"
+                ${HOME}/providerscripts/datastore/config/tooling/DeleteFromConfigDatastore.sh "${file_to_delete}" "no" "no"
+        fi
 }
 
 file_modified() {
         live_dir="${1}"
         modified_file="${2}"
 
-        /usr/bin/rsync -a --mkpath ${live_dir}${modified_file} `/bin/echo ${live_dir}${modified_file} | /bin/sed 's:/adt-config/:/adt-config-workarea/:'`
+        if ( [ -f ${live_dir}${modified_file} ] )
+        then
+                /usr/bin/rsync -a --mkpath ${live_dir}${modified_file} `/bin/echo ${live_dir}${modified_file} | /bin/sed 's:/adt-config/:/adt-config-workarea/:'`
+        fi
 }
 
 file_created() {
         live_dir="${1}"
         created_file="${2}"
 
-        /usr/bin/rsync -a --mkpath ${live_dir}${created_file} `/bin/echo ${live_dir}${created_file} | /bin/sed 's:/adt-config/:/adt-config-workarea/:'`
+        if ( [ -f ${live_dir}${created_file} ] )
+        then
+                /usr/bin/rsync -a --mkpath ${live_dir}${created_file} `/bin/echo ${live_dir}${created_file} | /bin/sed 's:/adt-config/:/adt-config-workarea/:'`
+        fi
 }
 
 /usr/bin/inotifywait -q -m -r -e modify,delete,create /var/lib/adt-config | while read DIRECTORY EVENT FILE 
