0ff9b577ef744c545456a6b99e26e8f28bd606db
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-15 01:03:53 +0000

    Update ActivateConfigDatastore.sh

--- providerscripts/datastore/config/ActivateConfigDatastore.sh
+++ providerscripts/datastore/config/ActivateConfigDatastore.sh
@@ -4,21 +4,37 @@ then
         ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "root" "/var/lib/adt-config"
 fi
 
-/usr/bin/inotifywait -q -m -r -e modify,delete,create /var/lib/adt-config | while read DIRECTORY EVENT FILE 
+monitor_for_datastore_changes() {
+while ( [ 1 ] )
 do
+        /bin/sleep 5
+        /bin/touch /tmp/lock
+        ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "root" "/var/lib/adt-config"
+        /bin/rm /tmp/lock
+done
+}
+
+monitor_for_datastore_changes &
+
+
+        /usr/bin/inotifywait -q -m -r -e modify,delete,create /var/lib/adt-config | while read DIRECTORY EVENT FILE 
+do
+        while ( [ -f /tmp/lock ] )
+        do
+                sleep 1
+        done
         case $EVENT in
                 MODIFY*)
-                       # file_modified "$DIRECTORY" "$FILE"
-                        ${HOME}/providerscripts/datastore/configwrapper/SyncToDatastoreWithoutDelete.sh "/var/lib/adt-config"
+                        # file_modified "$DIRECTORY" "$FILE"
+                        ${HOME}/providerscripts/datastore/configwrapper/SyncToConfigDatastoreWithoutDelete.sh "/var/lib/adt-config"
                         ;;
                 CREATE*)
-                       # file_created "$DIRECTORY" "$FILE"
-                        ${HOME}/providerscripts/datastore/configwrapper/SyncToDatastoreWithoutDelete.sh "/var/lib/adt-config"
+                        # file_created "$DIRECTORY" "$FILE"
+                        ${HOME}/providerscripts/datastore/configwrapper/SyncToConfigDatastoreWithoutDelete.sh "/var/lib/adt-config"
                         ;;
                 DELETE*)
-                       # file_removed "$DIRECTORY" "$FILE"
-                        ${HOME}/providerscripts/datastore/configwrapper/SyncFromDatastoreWithDelete.sh "root" "/var/lib/adt-config" "yes" 
-
+                        # file_removed "$DIRECTORY" "$FILE"
+                        ${HOME}/providerscripts/datastore/configwrapper/SyncToConfigDatastoreWithDelete.sh "root" "/var/lib/adt-config" "yes"  
                         ;;
         esac
 done
