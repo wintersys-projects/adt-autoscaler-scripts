eceaee2edd38f5e83561d13825e086c5fa68b910
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-19 16:36:15 +0000

    Update AuditSSHConnections.sh

--- utilities/security/AuditSSHConnections.sh
+++ utilities/security/AuditSSHConnections.sh
@@ -23,7 +23,7 @@
 
 HOME="`/bin/cat /home/homedir.dat`"
 
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh INSTALLED_SUCCESSFULLY`" = "" ] )
+if ( [ "`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh INSTALLED_SUCCESSFULLY`" = "" ] )
 then
         exit
 fi
