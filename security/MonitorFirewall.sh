c43d6e39a046984e2c8acc87878185ddf9129b40
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-29 16:41:39 +0000

    Update MonitorFirewall.sh

--- security/MonitorFirewall.sh
+++ security/MonitorFirewall.sh
@@ -32,7 +32,7 @@ then
 fi
 
 #If the toolkit isn't fully installed, we don't want to do anything, so exit
-if ( [ "`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh INSTALLED_SUCCESSFULLY`" = "" ] )
+if ( [ "`${HOME}/providerscripts/datastore/config/wrapper/ListFromDatastore.sh "config" "INSTALLED_SUCCESSFULLY"`" = "" ] )
 then
 	exit
 fi
