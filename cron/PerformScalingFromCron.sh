ccb76291dff139239cd729a636169197e4e07dc6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-29 16:40:48 +0000

    Update PerformScalingFromCron.sh

--- cron/PerformScalingFromCron.sh
+++ cron/PerformScalingFromCron.sh
@@ -22,7 +22,7 @@
 #########################################################################################
 #set -x
 #If we are here without being fully installed, exit
-if ( [ "`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh INSTALLED_SUCCESSFULLY`" = "" ] )
+if ( [ "`${HOME}/providerscripts/datastore/config/wrapper/ListFromDatastore.sh "config" "INSTALLED_SUCCESSFULLY"`" = "" ] )
 then
 	exit
 fi
