5fb821f6c164ce33081f8a5911bb18684bd40cf5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-29 15:40:17 +0000

    Create AgeOfDatastoreFile.sh

---
+++ providerscripts/datastore/config/wrapper/AgeOfDatastoreFile.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#####################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: This abstracts the finding the age of a datastore file so that ultimate 
+# implementation is not known by the calling script
+#####################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
+bucket_type="${1}"
+file_to_interrogate="${2}"
+
+if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "tool" ] )
+then
+    ${HOME}/providerscripts/datastore/operations/AgeOfDatastoreFile.sh "${bucket_type}" "${file_to_interrogate}"
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
+then
+:
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
+then
+:
+fi
