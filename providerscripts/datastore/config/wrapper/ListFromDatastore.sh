e777c6efbacac705ce88c8302113189b6347b032
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-03 12:32:13 +0000

    Update ListFromDatastore.sh

--- providerscripts/datastore/config/wrapper/ListFromDatastore.sh
+++ providerscripts/datastore/config/wrapper/ListFromDatastore.sh
@@ -30,6 +30,14 @@ then
         ${HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "${bucket_type}" "${file_to_list}" "${additional_specifier}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] || [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
+        if ( [ "`/bin/echo ${file_to_list} | /bin/grep 'STATIC_SCALE'`" ] )
+        then
+                live_one="`/bin/ls -Art /var/lib/adt-config/STATIC_SCALE* | /usr/bin/tail -n 1`"
+                /bin/mv ${live_one} /tmp
+                /bin/rm /var/lib/adt-config/*STATIC_SCALE*
+                /bin/mv /tmp/*STATIC_SCALE* /var/lib/adt-config
+        fi
+        
         if ( [ -f /var/lib/adt-config/${file_to_list} ] )
         then
                 /bin/ls /var/lib/adt-config/${file_to_list} | /usr/bin/awk -F'/' '{print $NF}'
