4a50cf0426d55fc3c15d134b48db8623739f9428
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-30 23:12:59 +0100

    Update as.sh

--- as.sh
+++ as.sh
@@ -161,9 +161,9 @@ ${HOME}/providerscripts/email/SendEmail.sh "A NEW AUTOSCALER HAS BEEN SUCCESSFUL
 /bin/touch ${HOME}/runtime/AUTOSCALER_READY
 /bin/touch ${HOME}/runtime/AUTHORISED_TO_SCALE
 
-#/bin/echo "${0} Enforcing permissions"
-#${HOME}/utilities/security/EnforcePermissions.sh &
+/bin/echo "${0} Enforcing permissions"
+${HOME}/utilities/security/EnforcePermissions.sh &
 
-${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &
+#${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &
 
 
